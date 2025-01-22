93fbe81c164e238d918af002c7be8fa52b6a69bd
RustBad <rustambad1993@gmail.com>
2025-01-22 20:35:24 +0500

    BJS2-53966 Cross-review

--- src/main/java/school/faang/user_service/controller/mentorship/MentorshipController.java
+++ src/main/java/school/faang/user_service/controller/mentorship/MentorshipController.java
@@ -1,18 +1,49 @@
 package school.faang.user_service.controller.mentorship;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.service.mentorship.MentorshipService;
 
 import java.util.List;
 
-@Controller
+@RequestMapping("/mentorship")
 @RequiredArgsConstructor
+@RestController
 public class MentorshipController {
 
-    private MentorshipService mentorshipService;
+    private final MentorshipService mentorshipService;
 
-    public List<Long> getMentees(long mentorId) {
-        return mentorshipService.getMentees(mentorId);
+    @GetMapping("/{userId}/mentees")
+    public List<Long> getMentees(@PathVariable Long userId) {
+        return mentorshipService.getMentees(userId);
+    }
+
+    @GetMapping("/{userId}/mentors")
+    public List<Long> getMentors(@PathVariable Long userId) {
+        return mentorshipService.getMentors(userId);
+    }
+
+    @DeleteMapping("/delete-mentee")
+    public ResponseEntity<String> deleteMentee(@RequestParam Long mentorId,
+                                               @RequestParam Long menteeId) {
+        mentorshipService.deleteMentee(mentorId, menteeId);
+        return ResponseEntity.ok().
+                body(String.format("The mentee id %s with the mentor id %s was successfully deleted",
+                        menteeId, mentorId));
+    }
+
+    @DeleteMapping("/delete-mentor")
+    public ResponseEntity<String> deleteMentor(@RequestParam Long mentorId,
+                                               @RequestParam Long menteeId) {
+        mentorshipService.deleteMentor(mentorId, menteeId);
+        return ResponseEntity.ok().
+                body(String.format("The mentor id %s with the mentee id %s was successfully deleted",
+                        mentorId, menteeId));
     }
 }
--- src/test/java/school/faang/user_service/service/mentorship/MentorshipServiceTest.java
+++ src/test/java/school/faang/user_service/service/mentorship/MentorshipServiceTest.java
@@ -5,18 +5,19 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.entity.User;
-import school.faang.user_service.mapper.UserMapperImpl;
 import school.faang.user_service.repository.mentorship.MentorshipRepository;
-
+import java.util.Collections;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 
@@ -29,56 +30,109 @@ public class MentorshipServiceTest {
     @Mock
     private MentorshipRepository mentorshipRepository;
 
-    @Spy
-    private UserMapperImpl userMapperIml;
-
-
-    private Long id = 2L;
-    private Long id2 = 4L;
-    private Long id3 = 3L;
-    private Long id4 = 5L;
-
-    private User user = new User();
-    private User user2 = new User();
-    private User user3 = new User();
-    private User user4 = new User();
-
-    private List<User> users;
-
-    private Optional<User> optionalUser;
-
+    private User user;
+    private User mentee;
+    private User mentor;
 
     @BeforeEach
-    public void setUp() {
-        user2.setId(id2);
-        user3.setId(id3);
-        user4.setId(id4);
-        users = List.of(user2, user3, user4);
+    void setup() {
+        user = new User();
+        user.setId(1L);
+        user.setUsername("user");
 
-        user.setId(id);
-        user.setMentees(users);
+        mentee = new User();
+        mentee.setId(2L);
+        mentee.setUsername("mentee");
 
-        optionalUser = Optional.of(user);
-    }
+        mentor = new User();
+        mentor.setId(3L);
+        mentor.setUsername("mentor");
 
-
-    @Test
-    public void testGetMentorshipNotNull() {
-        when(mentorshipRepository.findById(id)).thenReturn(optionalUser);
-        UserDto userDto = userMapperIml.toUserDto(optionalUser.get());
-
-        when(mentorshipRepository.findAllById(userDto.getMenteesIds())).thenReturn(users);
-
-        assertEquals(user.getMentees().size(), mentorshipService.getMentees(user.getId()).size());
-
+        user.setMentees(Collections.singletonList(mentee));
+        user.setMentors(List.of(mentor));
     }
 
     @Test
-    public void testGetMentorshipNull() {
-        when(mentorshipRepository.findById(id)).thenReturn(optionalUser);
+    void testGetMentees() {
+        when(mentorshipRepository.findById(user.getId())).thenReturn(Optional.of(user));
+        when(mentorshipRepository.findAllById(List.of(mentee.getId()))).thenReturn(List.of(mentee));
 
-        UserDto userDto = null;
+        List<Long> result = mentorshipService.getMentees(user.getId());
 
-        assertTrue(mentorshipService.getMentees(user.getId()).isEmpty());
+        assertEquals(List.of(mentee.getId()), result);
+    }
+
+    @Test
+    void testGetMentees_UserNotFound() {
+        when(mentorshipRepository.findById(user.getId())).thenReturn(Optional.empty());
+
+        List<Long> result = mentorshipService.getMentees(user.getId());
+
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void testGetMentors() {
+        when(mentorshipRepository.findById(user.getId())).thenReturn(Optional.of(user));
+        when(mentorshipRepository.findAllById(List.of(mentor.getId()))).thenReturn(List.of(mentor));
+
+        List<Long> result = mentorshipService.getMentors(user.getId());
+
+        assertEquals(List.of(mentor.getId()), result);
+    }
+
+    @Test
+    void testGetMentors_UserNotFound() {
+        when(mentorshipRepository.findById(user.getId())).thenReturn(Optional.empty());
+
+        List<Long> result = mentorshipService.getMentors(user.getId());
+
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void testDeleteMentee_MentorExistsAndMenteeExists() {
+        when(mentorshipRepository.findById(user.getId())).thenReturn(Optional.of(user));
+
+        mentorshipService.deleteMentee(user.getId(), mentee.getId());
+
+        verify(mentorshipRepository, Mockito.times(1)).delete(mentee);
+    }
+
+    @Test
+    void testDeleteMentee_MentorNotFound() {
+        when(mentorshipRepository.findById(user.getId())).thenThrow(NoSuchElementException.class);
+
+        assertThrows(NoSuchElementException.class, () -> mentorshipService.deleteMentee(user.getId(), mentee.getId()));
+    }
+
+    @Test
+    void testDeleteMentee_MenteeNotFound() {
+        when(mentorshipRepository.findById(user.getId())).thenReturn(Optional.of(user));
+
+        assertThrows(NoSuchElementException.class, () -> mentorshipService.deleteMentee(user.getId(), 999L));
+    }
+
+    @Test
+    void testDeleteMentor_MentorExistsAndMenteeExists() {
+        when(mentorshipRepository.findById(user.getId())).thenReturn(Optional.of(user));
+
+        mentorshipService.deleteMentor(mentor.getId(), user.getId());
+
+        verify(mentorshipRepository, Mockito.times(1)).delete(mentor);
+    }
+
+    @Test
+    void testDeleteMentor_MenteeNotFound() {
+        when(mentorshipRepository.findById(user.getId())).thenThrow(NoSuchElementException.class);
+
+        assertThrows(NoSuchElementException.class, () -> mentorshipService.deleteMentor(mentor.getId(), user.getId()));
+    }
+
+    @Test
+    void testDeleteMentor_MentorNotFound() {
+        when(mentorshipRepository.findById(user.getId())).thenReturn(Optional.of(user));
+
+        assertThrows(NoSuchElementException.class, () -> mentorshipService.deleteMentor(999L, user.getId()));
     }
 }
