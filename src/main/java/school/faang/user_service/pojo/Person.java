eb4664035ec6f0c346ea61a389f71ac73c4f8c60
Alexey Gorokhov <almigor@mail.ru>
2025-02-03 00:01:13 +0300

    добавил схему

--- src/main/java/school/faang/user_service/pojo/Person.java
+++ src/main/java/school/faang/user_service/pojo/Person.java
@@ -1,31 +1,56 @@
 package school.faang.user_service.pojo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class Person {
+    @JsonProperty("firstName")
     String firstName;
+    @JsonProperty("lastName")
     String lastName;
+    @JsonProperty("yearOfBirth")
     String yearOfBirth;
+    @JsonProperty("group")
     String group;
+    @JsonProperty("studentID")
     String studentID;
+    @JsonProperty("email")
     String email;
+    @JsonProperty("phone")
     String phone;
+    @JsonProperty("street")
     String street;
+    @JsonProperty("city")
     String city;
+    @JsonProperty("state")
     String state;
+    @JsonProperty("country")
     String country;
+    @JsonProperty("postalCode")
     String postalCode;
+    @JsonProperty("faculty")
     String faculty;
+    @JsonProperty("yearOfStudy")
     String yearOfStudy;
+    @JsonProperty("major")
     String major;
+    @JsonProperty("GPA")
     String GPA;
+    @JsonProperty("status")
     String status;
+    @JsonProperty("admissionDate")
     String admissionDate;
+    @JsonProperty("graduationDate")
     String graduationDate;
+    @JsonProperty("degree")
     String degree;
+    @JsonProperty("institution")
     String institution;
+    @JsonProperty("completionYear")
     String completionYear;
+    @JsonProperty("scholarship")
     String scholarship;
+    @JsonProperty("employer")
     String employer;
 }
