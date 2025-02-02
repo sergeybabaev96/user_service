b1189cc7801ad82fc1cf699010a633358e73c19e
Stepan Evangulov <stevangulov@gmail.com>
2025-02-02 11:55:45 +0200

    Feature-BJS2-54153-Steph-Integration-with-the-users-random-avatar-gen

--- src/test/java/school/faang/user_service/service/user/UserServiceTest.java
+++ src/test/java/school/faang/user_service/service/user/UserServiceTest.java
@@ -1,7 +1,9 @@
 package school.faang.user_service.service.user;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import school.faang.user_service.entity.Country;
 import school.faang.user_service.entity.User;
 import school.faang.user_service.repository.CountryRepository;
@@ -9,9 +11,13 @@ import school.faang.user_service.repository.UserRepository;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 
+@ExtendWith(MockitoExtension.class)
 class UserServiceTest {
     private final UserRepository userRepositoryMock = Mockito.mock(UserRepository.class);
     private final CountryRepository countryRepositoryMock = Mockito.mock(CountryRepository.class);
@@ -33,19 +39,5 @@ class UserServiceTest {
         User user = userService.registerUser("username", "email@example.com", "password", 1L);
         assertNotNull(user);
         assertEquals("username", user.getUsername());
-    }
-
-    @Test
-    void testDeactivateUser() {
-        User user = new User();
-        user.setId(1L);
-
-        Mockito.when(userRepositoryMock.findById(1L))
-                .thenReturn(Optional.of(user));
-        Mockito.when(userRepositoryMock.save(any(User.class)))
-                .thenAnswer(invocation -> invocation.getArgument(0));
-
-        assertDoesNotThrow(() -> userService.deactivateUser(1L));
-        assertFalse(user.isActive());
     }
 }
