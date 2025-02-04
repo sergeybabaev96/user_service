c0b973d737f6d295528cf6f0a58130f31497b7ac
Maria Bortsova <bortsovamariia@gmail.com>
2025-02-04 16:52:27 +0300

    BJS2-52933: add logs in exception handler

--- src/main/java/school/faang/user_service/controller/handler/GlobalExceptionHandler.java
+++ src/main/java/school/faang/user_service/controller/handler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package school.faang.user_service.controller.handler;
 
 import feign.FeignException;
 import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,6 +12,7 @@ import school.faang.user_service.dto.error.ErrorModel;
 import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.exception.ResourceNotFoundException;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -20,30 +22,35 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(DataValidationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorModel handleDataValidationException(DataValidationException ex) {
+        log.error("Data validation exception", ex);
         return createError(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorModel handleResourceNotFoundException(ResourceNotFoundException ex) {
+        log.error("Resource not found exception", ex);
         return createError(ex.getMessage(), HttpStatus.NOT_FOUND.value());
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorModel handleEntityNotFoundException(EntityNotFoundException ex) {
+        log.error("Entity not found exception", ex);
         return createError(ex.getMessage(), HttpStatus.NOT_FOUND.value());
     }
 
     @ExceptionHandler(FeignException.class)
     @ResponseStatus(HttpStatus.BAD_GATEWAY)
     public ErrorModel handleFeignException(FeignException ex) {
+        log.error("Feign exception", ex);
         return createError(ex.getMessage(), HttpStatus.BAD_GATEWAY.value());
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorModel handleGenericException(Exception ex) {
+        log.error("Internal server error exception", ex);
         return createError("An unexpected error occurred", HttpStatus.INTERNAL_SERVER_ERROR.value());
     }
 
