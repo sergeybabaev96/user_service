057b84c030c02cdb279e43c5fff790a0a7258b6f
AesaQ <ilyaovchinnikov2095@gmail.com>
2025-02-09 12:19:38 +0500

    added @Transactional

--- src/main/java/school/faang/user_service/service/event/EventService.java
+++ src/main/java/school/faang/user_service/service/event/EventService.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import school.faang.user_service.client.PromotionServiceClient;
 import school.faang.user_service.config.AppConfig;
 import school.faang.user_service.dto.event.EventDto;
@@ -146,6 +147,7 @@ public class EventService {
                 .toList();
     }
 
+    @Transactional
     public void removeAllPastEvents() {
         List<Event> pastEvents = eventRepository.findAll()
                 .stream()
