93179090d9b137a21068cf2da7d06e569291415b
Lexsandr <lexsandr@inbox.ru>
2025-02-10 19:39:31 +0300

    BJS2-52769 : 1) Return fix

--- src/test/java/school/faang/user_service/UserServiceApplicationTest.java
+++ src/test/java/school/faang/user_service/UserServiceApplicationTest.java
@@ -1,33 +1,33 @@
-//package school.faang.user_service;
-//
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.TestInstance;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.context.ApplicationContext;
-//import org.testcontainers.containers.PostgreSQLContainer;
-//import org.testcontainers.junit.jupiter.Container;
-//import org.testcontainers.junit.jupiter.Testcontainers;
-//
-//import static org.junit.jupiter.api.Assertions.assertNotNull;
-//
-//@SpringBootTest
-//@Testcontainers
-//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-//class UserServiceApplicationTest {
-//
-//    @Container
-//    private static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine")
-//            .withDatabaseName("mydb")
-//            .withUsername("user")
-//            .withPassword("password");
-//
-//    @Autowired
-//    private ApplicationContext context;
-//
-//    @Test
-//    void contextLoads() {
-//        postgres.start();
-//        assertNotNull(context, "Spring context not loaded");
-//    }
-//}
+package school.faang.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+@Testcontainers
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class UserServiceApplicationTest {
+
+    @Container
+    private static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine")
+            .withDatabaseName("mydb")
+            .withUsername("user")
+            .withPassword("password");
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Test
+    void contextLoads() {
+        postgres.start();
+        assertNotNull(context, "Spring context not loaded");
+    }
+}
