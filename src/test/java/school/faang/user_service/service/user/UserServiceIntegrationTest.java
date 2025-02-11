13e868f46e20761ff62902b30bfa6af38e422f7d
Pavel <45390423+rsticks@users.noreply.github.com>
2025-02-11 21:44:02 +0300

    query acceleration

--- src/test/java/school/faang/user_service/service/user/UserServiceIntegrationTest.java
+++ src/test/java/school/faang/user_service/service/user/UserServiceIntegrationTest.java
@@ -16,11 +16,13 @@ import school.faang.user_service.mapper.TariffMapper;
 import school.faang.user_service.repository.TariffRepository;
 import school.faang.user_service.repository.UserRepository;
 import school.faang.user_service.service.UserService;
-import school.faang.user_service.service.tariff.TariffService;
+import school.faang.user_service.service.tariff.TariffServiceImpl;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -31,7 +33,7 @@ public class UserServiceIntegrationTest extends BaseTest {
     private UserService userService;
 
     @SpyBean
-    private TariffService tariffService;
+    private TariffServiceImpl tariffService;
     @MockBean
     private UserRepository userRepository;
     @MockBean
@@ -45,7 +47,7 @@ public class UserServiceIntegrationTest extends BaseTest {
     }
 
     @Test
-    public void findUsersByFilterDecrementShows() {
+    public void findUsersByFilterDecrementShows() throws InterruptedException {
 
         GetUserRequest request = new GetUserRequest();
         request.setFilter(UserDto.builder().build());
@@ -63,12 +65,12 @@ public class UserServiceIntegrationTest extends BaseTest {
                                 .id(1L)
                                 .tariff(tariff)
                         .build()));
-        when(tariffRepository.existsById(1L)).thenReturn(true);
+        when(tariffRepository.findById(1L)).thenReturn(Optional.of(tariff));
 
         userService.findUsersByFilter(request);
 
-        verify(tariffService).decrementShows(tariff.getId());
-        verify(tariffRepository).save(Tariff.builder()
+        verify(tariffService, timeout(100)).decrementShows(tariff.getId());
+        verify(tariffRepository, timeout(100)).save(Tariff.builder()
                 .isActive(false)
                 .id(1L)
                 .shows(0)
