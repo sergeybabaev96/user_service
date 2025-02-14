f3cd91accd233258075b44d0de0bf06b9bf83fe5
Maria Bortsova <bortsovamariia@gmail.com>
2025-02-14 12:14:48 +0300

    BJS-53133: resolve conflicts

--- src/main/java/school/faang/user_service/service/user/UserBanServiceImpl.java
+++ src/main/java/school/faang/user_service/service/user/UserBanServiceImpl.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import school.faang.user_service.entity.User;
-import school.faang.user_service.repository.UserRepository;
+import school.faang.user_service.repository.user.UserRepository;
 
 import java.util.Optional;
 
