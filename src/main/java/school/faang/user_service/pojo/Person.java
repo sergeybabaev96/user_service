3351fc83ffa25815b7502f79ebc5980c0b74a81a
Alexey Gorokhov <almigor@mail.ru>
2025-02-14 22:52:16 +0300

    Исправление после ревью

--- src/main/java/school/faang/user_service/pojo/Person.java
+++ src/main/java/school/faang/user_service/pojo/Person.java
@@ -5,54 +5,32 @@ import lombok.Data;
 
 @Data
 public class Person {
-    @JsonProperty("firstName")
-    String firstName;
-    @JsonProperty("lastName")
-    String lastName;
-    @JsonProperty("yearOfBirth")
-    String yearOfBirth;
-    @JsonProperty("group")
-    String group;
+    private String firstName;
+    private String lastName;
+    private String yearOfBirth;
+    private String group;
     @JsonProperty("studentID")
-    String studentID;
-    @JsonProperty("email")
-    String email;
-    @JsonProperty("phone")
-    String phone;
-    @JsonProperty("street")
-    String street;
-    @JsonProperty("city")
-    String city;
-    @JsonProperty("state")
-    String state;
-    @JsonProperty("country")
-    String country;
-    @JsonProperty("postalCode")
-    String postalCode;
-    @JsonProperty("faculty")
-    String faculty;
-    @JsonProperty("yearOfStudy")
-    String yearOfStudy;
-    @JsonProperty("major")
-    String major;
+    private String studentId;
+    private String email;
+    private String phone;
+    private String street;
+    private String city;
+    private String state;
+    private String country;
+    private String postalCode;
+    private String faculty;
+    private String yearOfStudy;
+    private String major;
     @JsonProperty("GPA")
-    String GPA;
-    @JsonProperty("status")
-    String status;
-    @JsonProperty("admissionDate")
-    String admissionDate;
-    @JsonProperty("graduationDate")
-    String graduationDate;
-    @JsonProperty("degree")
-    String degree;
-    @JsonProperty("institution")
-    String institution;
-    @JsonProperty("completionYear")
-    String completionYear;
-    @JsonProperty("scholarship")
-    String scholarship;
-    @JsonProperty("employer")
-    String employer;
+    private String gpa;
+    private String status;
+    private String admissionDate;
+    private String graduationDate;
+    private String degree;
+    private String institution;
+    private String completionYear;
+    private String scholarship;
+    private String employer;
 
     public String toString() {
         return String.format(
