13eada39f73eb8383eaa4ce28111d1f00b6706db
lehaps <awda.96@bk.ru>
2025-02-15 21:39:55 +0300

    Не прошёл тест

--- src/test/java/school/faang/user_service/service/SkillServiceTest.java
+++ src/test/java/school/faang/user_service/service/SkillServiceTest.java
@@ -88,154 +88,161 @@ class SkillServiceTest {
                 .toList();
     }
 
-//    @Test
-//    public void shouldSuccessCreate() {
-//        doNothing().when(skillValidator).validateSkill(any(SkillDto.class));
-//
-//        when(skillMapper.toEntity(skillDto)).thenReturn(skill);
-//        when(userService.getAllByIds(skillDto.userIds())).thenReturn(Collections.emptyList());
-//        when(skillRepository.save(skill)).thenReturn(skill);
-//        when(skillMapper.toDto(skill)).thenReturn(skillDto);
-//
-//        SkillDto createdSkillDto = skillService.create(skillDto);
-//
-//        verify(skillValidator).validateSkill(skillDto);
-//        verify(skillMapper).toEntity(skillDto);
-//        verify(userService).getAllByIds(skillDto.userIds());
-//        verify(skillRepository).save(skill);
-//        verify(skillMapper).toDto(skill);
-//
-//        Assertions.assertEquals(skillDto, createdSkillDto);
-//    }
-//
-//    @Test
-//    public void shouldSuccessGetUsersSkills() {
-//        when(skillRepository.findAllByUserId(USER_ID)).thenReturn(skills);
-//        skills.forEach(skill1 -> when(skillMapper.toDto(skill1))
-//                .thenReturn(SkillDto.builder()
-//                        .id(skill1.getId())
-//                        .build()));
-//
-//        List<SkillDto> result = skillService.getUsersSkills(USER_ID);
-//
-//        Assertions.assertEquals(skills.size(), result.size());
-//        IntStream.range(0, result.size())
-//                .forEach(i -> Assertions.assertEquals(result.get(i), skillsDto.get(i)));
-//
-//        verify(skillRepository).findAllByUserId(USER_ID);
-//    }
-//
-//    @Test
-//    public void shouldSuccessGetOfferedSkills() {
-//        when(skillRepository.findSkillsOfferedToUser(USER_ID)).thenReturn(skills);
-//        when(skillRepository.findSkillsOfferedToUser(USER_ID)).thenReturn(skills);
-//
-//        List<SkillCandidateDto> candidatesDto = IntStream.range(0, skills.size())
-//                .mapToObj(i -> SkillCandidateDto.builder()
-//                        .skillDto(skillsDto.get(i))
-//                        .offersAmount(OFFERS_AMOUNT)
-//                        .build())
-//                .toList();
-//
-//        IntStream.range(0, skills.size()).forEach(i -> {
-//            Skill skill = skills.get(i);
-//            when(skillOfferRepository
-//                    .countAllOffersOfSkill(skill.getId(), USER_ID)).thenReturn(OFFERS_AMOUNT);
-//            when(skillCandidateMapper
-//                    .toDto(skill, OFFERS_AMOUNT)).thenReturn(candidatesDto.get(i));
-//        });
-//
-//        List<SkillCandidateDto> result = skillService.getOfferedSkills(USER_ID);
-//
-//        Assertions.assertEquals(skills.size(), result.size());
-//        IntStream.range(0, skills.size())
-//                .forEach(i -> Assertions.assertEquals(candidatesDto.get(i), result.get(i)));
-//
-//        verify(skillRepository).findSkillsOfferedToUser(USER_ID);
-//        IntStream.range(0, skills.size()).forEach(i -> {
-//            Skill skill = skills.get(i);
-//            verify(skillOfferRepository).countAllOffersOfSkill(skill.getId(), USER_ID);
-//            verify(skillCandidateMapper).toDto(skill, OFFERS_AMOUNT);
-//        });
-//    }
-//
-//    @Test
-//    public void shouldUserHasSkillInvalid(){
-//        when(skillRepository.findUserSkill(SKILL_ID, USER_ID)).thenReturn(Optional.of(skill));
-//
-//        BusinessException exception = Assertions.assertThrows(BusinessException.class,
-//                () -> skillService.acquireSkillFromOffers(SKILL_ID, USER_ID));
-//        Assertions.assertEquals(MessageError.USER_ALREADY_HAS_SUGGESTED_SKILL.name(), exception.getMessage());
-//
-//        verify(skillRepository).findUserSkill(SKILL_ID, USER_ID);
-//        verifyNoInteractions(skillOfferRepository, skillMapper);
-//    }
-//
-//    @Test
-//    public void shouldNotEnoughSkillOffersInvalid(){
-//        when(skillRepository.findUserSkill(SKILL_ID, USER_ID)).thenReturn(Optional.empty());
-//        when(userService.getById(USER_ID)).thenReturn(User.builder().build());
-//        when(skillOfferRepository.findAllOffersOfSkill(SKILL_ID, USER_ID))
-//                .thenReturn(Collections.emptyList());
-//
-//        BusinessException exception = Assertions.assertThrows(BusinessException.class,
-//                () -> skillService.acquireSkillFromOffers(SKILL_ID, USER_ID));
-//        Assertions.assertEquals(MessageError.NOT_ENOUGH_SKILL_OFFERS.name(), exception.getMessage());
-//
-//        verify(skillOfferRepository).findAllOffersOfSkill(SKILL_ID, USER_ID);
-//        verify(userService).getById(USER_ID);
-//        verify(skillRepository).findUserSkill(SKILL_ID, USER_ID);
-//        verifyNoInteractions(skillMapper);
-//    }
-//
-//    @Test
-//    public void shouldNotSkillAvailableInvalid(){
-//        when(skillRepository.findUserSkill(SKILL_ID, USER_ID)).thenReturn(Optional.empty());
-//        when(userService.getById(USER_ID)).thenReturn(User.builder().build());
-//
-//        List<SkillOffer> offers = Collections.nCopies(MIN_SKILL_OFFERS, new SkillOffer());
-//
-//        when(skillOfferRepository.findAllOffersOfSkill(SKILL_ID, USER_ID)).thenReturn(offers);
-//        doNothing().when(skillRepository).assignSkillToUser(SKILL_ID, USER_ID);
-//
-//        BusinessException exception = Assertions.assertThrows(BusinessException.class,
-//                () -> skillService.acquireSkillFromOffers(SKILL_ID, USER_ID));
-//        Assertions.assertEquals(MessageError.SKILL_NOT_AVAILABLE.name(), exception.getMessage());
-//
-//        verify(skillOfferRepository).findAllOffersOfSkill(SKILL_ID, USER_ID);
-//        verify(userService).getById(USER_ID);
-//        verify(skillRepository, times(2)).findUserSkill(SKILL_ID, USER_ID);
-//        verifyNoInteractions(skillMapper);
-//    }
-//
-//    @Test
-//    public void shouldSuccessAcquireSkillFromOffers(){
-//        when(skillRepository.findUserSkill(SKILL_ID, USER_ID))
-//                .thenReturn(Optional.empty())
-//                .thenReturn(Optional.of(skill));
-//        when(userService.getById(USER_ID)).thenReturn(user);
-//
-//        List<SkillOffer> offers = Collections.nCopies(MIN_SKILL_OFFERS, SkillOffer.builder()
-//                .skill(skill)
-//                .build());
-//
-//        when(skillOfferRepository.findAllOffersOfSkill(SKILL_ID, USER_ID)).thenReturn(offers);
-//        doNothing().when(skillRepository).assignSkillToUser(SKILL_ID, USER_ID);
-//
-//        when(skillMapper.toDto(skill)).thenReturn(skillDto);
-//        when(guaranteeRepository.save(any(UserSkillGuarantee.class)))
-//                .thenAnswer(invocation -> invocation.getArgument(0));
-//
-//        SkillDto result = skillService.acquireSkillFromOffers(SKILL_ID, USER_ID);
-//
-//        Assertions.assertEquals(skillDto, result);
-//
-//        verify(skillRepository, times(2)).findUserSkill(SKILL_ID, USER_ID);
-//        verify(userService, times(1)).getById(USER_ID);
-//        verify(skillOfferRepository, times(1)).findAllOffersOfSkill(SKILL_ID, USER_ID);
-//        verify(skillRepository, times(1)).assignSkillToUser(SKILL_ID, USER_ID);
-//        verify(skillRepository, times(2)).findUserSkill(SKILL_ID, USER_ID);
-//        verify(skillMapper, times(1)).toDto(skill);
-//        verify(guaranteeRepository,times(offers.size())).save(any(UserSkillGuarantee.class));
-//    }
+    @Test
+    public void shouldSuccessCreate() {
+        doNothing().when(skillValidator).validateSkill(any(SkillDto.class));
+
+        when(skillMapper.toEntity(skillDto)).thenReturn(skill);
+        when(userService.getAllByIds(skillDto.userIds())).thenReturn(Collections.emptyList());
+        when(skillRepository.save(skill)).thenReturn(skill);
+        when(skillMapper.toDto(skill)).thenReturn(skillDto);
+
+        SkillDto createdSkillDto = skillService.create(skillDto);
+
+        verify(skillValidator).validateSkill(skillDto);
+        verify(skillMapper).toEntity(skillDto);
+        verify(userService).getAllByIds(skillDto.userIds());
+        verify(skillRepository).save(skill);
+        verify(skillMapper).toDto(skill);
+
+        Assertions.assertEquals(skillDto, createdSkillDto);
+    }
+
+    @Test
+    public void shouldSuccessGetUsersSkills() {
+        when(skillRepository.findAllByUserId(USER_ID)).thenReturn(skills);
+        skills.forEach(skill1 -> when(skillMapper.toDto(skill1))
+                .thenReturn(SkillDto.builder()
+                        .id(skill1.getId())
+                        .build()));
+
+        List<SkillDto> result = skillService.getUsersSkills(USER_ID);
+
+        Assertions.assertEquals(skills.size(), result.size());
+        IntStream.range(0, result.size())
+                .forEach(i -> Assertions.assertEquals(result.get(i), skillsDto.get(i)));
+
+        verify(skillRepository).findAllByUserId(USER_ID);
+    }
+
+    @Test
+    public void shouldSuccessGetOfferedSkills() {
+        when(skillRepository.findSkillsOfferedToUser(USER_ID)).thenReturn(skills);
+        when(skillRepository.findSkillsOfferedToUser(USER_ID)).thenReturn(skills);
+
+        List<SkillCandidateDto> candidatesDto = IntStream.range(0, skills.size())
+                .mapToObj(i -> SkillCandidateDto.builder()
+                        .skillDto(skillsDto.get(i))
+                        .offersAmount(OFFERS_AMOUNT)
+                        .build())
+                .toList();
+
+        IntStream.range(0, skills.size()).forEach(i -> {
+            Skill skill = skills.get(i);
+            when(skillOfferRepository
+                    .countAllOffersOfSkill(skill.getId(), USER_ID)).thenReturn(OFFERS_AMOUNT);
+            when(skillCandidateMapper
+                    .toDto(skill, OFFERS_AMOUNT)).thenReturn(candidatesDto.get(i));
+        });
+
+        List<SkillCandidateDto> result = skillService.getOfferedSkills(USER_ID);
+
+        Assertions.assertEquals(skills.size(), result.size());
+        IntStream.range(0, skills.size())
+                .forEach(i -> Assertions.assertEquals(candidatesDto.get(i), result.get(i)));
+
+        verify(skillRepository).findSkillsOfferedToUser(USER_ID);
+        IntStream.range(0, skills.size()).forEach(i -> {
+            Skill skill = skills.get(i);
+            verify(skillOfferRepository).countAllOffersOfSkill(skill.getId(), USER_ID);
+            verify(skillCandidateMapper).toDto(skill, OFFERS_AMOUNT);
+        });
+    }
+
+    @Test
+    public void shouldUserHasSkillInvalid(){
+        when(skillRepository.findUserSkill(SKILL_ID, USER_ID)).thenReturn(Optional.of(skill));
+
+        BusinessException exception = Assertions.assertThrows(BusinessException.class,
+                () -> skillService.acquireSkillFromOffers(SKILL_ID, USER_ID));
+        Assertions.assertEquals(MessageError.USER_ALREADY_HAS_SUGGESTED_SKILL.name(), exception.getMessage());
+
+        verify(skillRepository).findUserSkill(SKILL_ID, USER_ID);
+        verifyNoInteractions(skillOfferRepository, skillMapper);
+    }
+
+    @Test
+    public void shouldNotEnoughSkillOffersInvalid(){
+        when(skillRepository.findUserSkill(SKILL_ID, USER_ID)).thenReturn(Optional.empty());
+        when(userService.getById(USER_ID)).thenReturn(User.builder().build());
+        when(skillOfferRepository.findAllOffersOfSkill(SKILL_ID, USER_ID))
+                .thenReturn(Collections.emptyList());
+
+        BusinessException exception = Assertions.assertThrows(BusinessException.class,
+                () -> skillService.acquireSkillFromOffers(SKILL_ID, USER_ID));
+        Assertions.assertEquals(MessageError.NOT_ENOUGH_SKILL_OFFERS.name(), exception.getMessage());
+
+        verify(skillOfferRepository).findAllOffersOfSkill(SKILL_ID, USER_ID);
+        verify(userService).getById(USER_ID);
+        verify(skillRepository).findUserSkill(SKILL_ID, USER_ID);
+        verifyNoInteractions(skillMapper);
+    }
+
+    @Test
+    public void shouldNotSkillAvailableInvalid(){
+        when(skillRepository.findUserSkill(SKILL_ID, USER_ID)).thenReturn(Optional.empty());
+        when(userService.getById(USER_ID)).thenReturn(User.builder().build());
+
+        List<SkillOffer> offers = Collections.nCopies(MIN_SKILL_OFFERS, new SkillOffer());
+
+        when(skillOfferRepository.findAllOffersOfSkill(SKILL_ID, USER_ID)).thenReturn(offers);
+        doNothing().when(skillRepository).assignSkillToUser(SKILL_ID, USER_ID);
+
+        BusinessException exception = Assertions.assertThrows(BusinessException.class,
+                () -> skillService.acquireSkillFromOffers(SKILL_ID, USER_ID));
+        Assertions.assertEquals(MessageError.SKILL_NOT_AVAILABLE.name(), exception.getMessage());
+
+        verify(skillOfferRepository).findAllOffersOfSkill(SKILL_ID, USER_ID);
+        verify(userService).getById(USER_ID);
+        verify(skillRepository, times(2)).findUserSkill(SKILL_ID, USER_ID);
+        verifyNoInteractions(skillMapper);
+    }
+
+    @Test
+    public void shouldSuccessAcquireSkillFromOffers(){
+        when(skillRepository.findUserSkill(SKILL_ID, USER_ID))
+                .thenReturn(Optional.empty())
+                .thenReturn(Optional.of(skill));
+        when(userService.getById(USER_ID)).thenReturn(user);
+
+        List<SkillOffer> offers = Collections.nCopies(MIN_SKILL_OFFERS, SkillOffer.builder()
+                .skill(skill)
+                .build());
+
+        when(skillOfferRepository.findAllOffersOfSkill(SKILL_ID, USER_ID)).thenReturn(offers);
+        doNothing().when(skillRepository).assignSkillToUser(SKILL_ID, USER_ID);
+
+        when(skillMapper.toDto(skill)).thenReturn(skillDto);
+        when(guaranteeRepository.save(any(UserSkillGuarantee.class)))
+                .thenAnswer(invocation -> invocation.getArgument(0));
+
+        SkillDto result = skillService.acquireSkillFromOffers(SKILL_ID, USER_ID);
+
+        Assertions.assertEquals(skillDto, result);
+
+        verify(skillRepository, times(2)).findUserSkill(SKILL_ID, USER_ID);
+        verify(userService, times(1)).getById(USER_ID);
+        verify(skillOfferRepository, times(1)).findAllOffersOfSkill(SKILL_ID, USER_ID);
+        verify(skillRepository, times(1)).assignSkillToUser(SKILL_ID, USER_ID);
+        verify(skillRepository, times(2)).findUserSkill(SKILL_ID, USER_ID);
+        verify(skillMapper, times(1)).toDto(skill);
+        verify(guaranteeRepository,times(offers.size())).save(any(UserSkillGuarantee.class));
+    }
+
+
+    @Test
+    void testAddition() {
+        int result = 2 + 2;
+        Assertions.assertEquals(5, result, "Addition should result in 4, not 5"); // Неправильное утверждение!
+    }
 }
