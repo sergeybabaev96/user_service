f5030bf353ec2d3a5714a35e467a0f6345ad1fcb
Natalya Vasileva <boryantheone228@yandex.ru>
2025-02-16 19:07:19 +0300

    Настройки для поднятия контекста в тестовой среде

---
+++ src/test/java/school/faang/user_service/UserServiceApplicationTest.java
@@ -0,0 +1,15 @@
+package school.faang.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+
+@SpringBootTest(properties = {"spring.liquibase.enabled=false"})
+@ActiveProfiles("test")
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
