3fcf36ce50d5b8d81827cf5671876119664eda96
RustBad <rustambad1993@gmail.com>
2025-02-18 10:04:22 +0500

    BJS2-53966

--- src/main/java/school/faang/user_service/exception/GlobalExceptionHandler.java
+++ src/main/java/school/faang/user_service/exception/GlobalExceptionHandler.java
@@ -57,4 +57,9 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<>(response, errorMessage.getStatus());
     }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 }
--- src/main/java/school/faang/user_service/service/MentorshipService.java
+++ src/main/java/school/faang/user_service/service/MentorshipService.java
@@ -2,12 +2,18 @@ package school.faang.user_service.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import school.faang.user_service.entity.User;
 import school.faang.user_service.repository.UserRepository;
 import school.faang.user_service.repository.mentorship.MentorshipRepository;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -39,5 +45,26 @@ public class MentorshipService {
                 .peek(goal -> goal.setMentor(mentee))
                 .toList());
         userRepository.save(mentee);
+    }
+
+
+    @Transactional(readOnly = true)
+    public List<Long> getMentees(Long userId) {
+        return mentorshipRepository.findMenteeIdsByMentorId(userId);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Long> getMentors(Long userId) {
+        return mentorshipRepository.findMentorIdsByMenteeId(userId);
+    }
+
+    @Transactional
+    public void deleteMentee(Long mentorId, Long menteeId) {
+        mentorshipRepository.deleteByMentorIdAndMenteeId(mentorId, menteeId);
+    }
+
+    @Transactional
+    public void deleteMentor(Long menteeId, Long mentorId) {
+        mentorshipRepository.deleteByMenteeIdAndMentorId(menteeId, mentorId);
     }
 }
