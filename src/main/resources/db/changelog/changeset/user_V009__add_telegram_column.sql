4777b5cf8565dd3d880436961fa7e66a12dbec53
narol01 <kmingogod@gmail.com>
2025-02-27 12:48:27 +0100

    user-service-for-telegram-fix-test-BJS2-54566-narol

--- src/main/resources/db/changelog/changeset/user_V009__add_telegram_column.sql
+++ src/main/resources/db/changelog/changeset/user_V009__add_telegram_column.sql
@@ -1,4 +1,4 @@
 ALTER TABLE users
-    ADD COLUMN telegram_username,
-    ADD COLUMN telegram_chat_id,
+    ADD COLUMN telegram_username varchar,
+    ADD COLUMN telegram_chat_id varchar,
     ADD COLUMN preference INT;
