632455be18813dc779042e31e5e8c354a74401c8
Pavel <45390423+rsticks@users.noreply.github.com>
2025-02-28 16:01:03 +0300

    BJS2-53205 рефакторинг

--- src/main/java/school/faang/user_service/redis/RedisEventPublisher.java
+++ src/main/java/school/faang/user_service/redis/RedisEventPublisher.java
@@ -1,7 +1,10 @@
 package school.faang.user_service.redis;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.QueryTimeoutException;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 import school.faang.user_service.redis.event.RedisEvent;
 
@@ -11,6 +14,7 @@ public class RedisEventPublisher implements MessagePublisher {
     private final RedisTemplate<String, Object> redisTemplate;
 
     @Override
+    @Retryable(retryFor = QueryTimeoutException.class, backoff = @Backoff(delay = 2000, multiplier = 2))
     public void publish(RedisEvent event, String topic) {
         redisTemplate.convertAndSend(topic, event);
     }
