6991f9b2217b4fd1dc7232f4824ca967b087e8ad
VladimirGontarenko <mister.gontarenko@yandex.ru>
2025-02-28 10:52:18 +0400

    feature/BJS2-52482-deleting-past-events-fix

--- src/main/java/school/faang/user_service/config/ExecutorConfig.java
+++ src/main/java/school/faang/user_service/config/ExecutorConfig.java
@@ -6,9 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
