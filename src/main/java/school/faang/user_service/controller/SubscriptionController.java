34004525f8ff6ca8149fb25e2b1ebd83a1cc2392
Artemon <artemon.kos@outlook.com>
2025-03-02 22:53:19 +0100

    add getFollowers feature

--- src/main/java/school/faang/user_service/controller/SubscriptionController.java
+++ src/main/java/school/faang/user_service/controller/SubscriptionController.java
@@ -2,14 +2,33 @@ package school.faang.user_service.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import school.faang.user_service.dto.UserDto;
+import school.faang.user_service.dto.UserFilterDto;
+import school.faang.user_service.entity.UserFilter;
 import school.faang.user_service.service.SubscriptionService;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class SubscriptionController {
-    private SubscriptionService subscriptionService;
+    private final SubscriptionService subscriptionService;
 
     public void followUser(long followerId, long targetId) {
         subscriptionService.followUser(followerId, targetId);
+    }
+
+    public List<UserDto> getFollowers(long targetId, UserFilterDto filter) {
+        return subscriptionService.getFollowers(targetId, new UserFilter(
+                        filter.namePattern()
+                        , filter.phonePattern()
+                        , filter.experienceMin()
+                        , filter.experienceMax()))
+                .stream()
+                .map(user -> new UserDto(
+                        user.getId()
+                        , user.getUsername()
+                        , user.getEmail()))
+                .toList();
     }
 }
