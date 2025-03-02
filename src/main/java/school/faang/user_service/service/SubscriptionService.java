f5e6bcc72ca7fc0b3ac3943d511c357e31fce8a4
Artemon <artemon.kos@outlook.com>
2025-03-02 21:37:25 +0100

    unsubscribe feature

--- src/main/java/school/faang/user_service/service/SubscriptionService.java
+++ src/main/java/school/faang/user_service/service/SubscriptionService.java
@@ -11,12 +11,19 @@ public class SubscriptionService {
     private SubscriptionRepository subscriptionRepository;
 
     public void followUser(long followerId, long targetId) {
-        {   //validation
-            if (followerId == targetId)
-                throw new DataValidationException("A user cannot follow themselves. UserId: " + targetId);
-            if (subscriptionRepository.existsByFollowerIdAndFolloweeId(followerId, targetId))
-                throw new DataValidationException("The subscription has already been issued");
-        }
+        ensureSubscriptionStateValidation(followerId, targetId, false);
         subscriptionRepository.followUser(followerId, targetId);
+    }
+
+    public void unfollowUser(long followerId, long targetId) {
+        ensureSubscriptionStateValidation(followerId, targetId, true);
+        subscriptionRepository.unfollowUser(followerId, targetId);
+    }
+
+    private void ensureSubscriptionStateValidation(long followerId, long targetId, boolean shouldExist) {
+        if (followerId == targetId)
+            throw new DataValidationException("A user cannot follow themselves. UserId: " + targetId);
+        if (subscriptionRepository.existsByFollowerIdAndFolloweeId(followerId, targetId) != shouldExist)
+            throw new DataValidationException("The subscription has already been issued");
     }
 }
