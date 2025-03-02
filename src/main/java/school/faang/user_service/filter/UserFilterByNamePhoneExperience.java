1772229811610fb881d87bd2544ac7e08abc68ef
Artemon <artemon.kos@outlook.com>
2025-03-02 23:28:13 +0100

    refactor controller with mappers

--- src/main/java/school/faang/user_service/filter/UserFilterByNamePhoneExperience.java
+++ src/main/java/school/faang/user_service/filter/UserFilterByNamePhoneExperience.java
@@ -5,25 +5,15 @@ import school.faang.user_service.entity.User;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
 
-public class UserFilter implements Predicate<User> {
-    private int experienceMin;
-    private int experienceMax;
-    private boolean experienceCheck = false;
+public class UserFilterByNamePhoneExperience implements Predicate<User> {
+    private final int experienceMin;
+    private final int experienceMax;
     private final Pattern namePattern;
     private final Pattern phonePattern;
 
-    public UserFilter(String nameRegex, String phoneRegex) {
+    public UserFilterByNamePhoneExperience(String nameRegex, String phoneRegex, int experienceMin, int experienceMax) {
         this.namePattern = nameRegex == null || nameRegex.isBlank() ? null : Pattern.compile(nameRegex);
         this.phonePattern = phoneRegex == null || phoneRegex.isBlank() ? null : Pattern.compile(phoneRegex);
-    }
-
-    public UserFilter(int experienceMin, int experienceMax) {
-        this(null, null, experienceMin, experienceMax);
-    }
-
-    public UserFilter(String nameRegex, String phoneRegex, int experienceMin, int experienceMax) {
-        this(nameRegex, phoneRegex);
-        this.experienceCheck = true;
         this.experienceMin = experienceMin;
         this.experienceMax = experienceMax;
     }
@@ -34,8 +24,7 @@ public class UserFilter implements Predicate<User> {
 
         boolean matchesName = (namePattern == null) || namePattern.matcher(user.getUsername()).matches();
         boolean matchesPhone = (phonePattern == null) || phonePattern.matcher(user.getPhone()).matches();
-        boolean experienceInRange = !experienceCheck ||
-                (user.getExperience() >= experienceMin && user.getExperience() <= experienceMax);
+        boolean experienceInRange = user.getExperience() >= experienceMin && user.getExperience() <= experienceMax;
 
         return matchesName && matchesPhone && experienceInRange;
     }
