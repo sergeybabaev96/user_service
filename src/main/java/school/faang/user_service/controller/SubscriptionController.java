70f2541a200b10c9d273aebaa82170544bdc930b
Artemon <artemon.kos@outlook.com>
2025-03-02 21:24:10 +0100

    clear import

--- src/main/java/school/faang/user_service/controller/SubscriptionController.java
+++ src/main/java/school/faang/user_service/controller/SubscriptionController.java
@@ -1,11 +1,8 @@
 package school.faang.user_service.controller;
 
-import jakarta.xml.bind.ValidationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import school.faang.user_service.service.SubscriptionService;
-
-import java.util.Objects;
 
 @Controller
 @RequiredArgsConstructor
