c6c56634d25af40a65cbca0d87e88990818edd48
Artemon <artemon.kos@outlook.com>
2025-03-02 21:22:32 +0100

    fitch user subscription

--- src/main/java/school/faang/user_service/service/SubscriptionService.java
+++ src/main/java/school/faang/user_service/service/SubscriptionService.java
@@ -1,4 +1,22 @@
 package school.faang.user_service.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import school.faang.user_service.exception.DataValidationException;
+import school.faang.user_service.repository.SubscriptionRepository;
+
+@Service
+@RequiredArgsConstructor
 public class SubscriptionService {
+    private SubscriptionRepository subscriptionRepository;
+
+    public void followUser(long followerId, long targetId) {
+        {   //validation
+            if (followerId == targetId)
+                throw new DataValidationException("A user cannot follow themselves. UserId: " + targetId);
+            if (subscriptionRepository.existsByFollowerIdAndFolloweeId(followerId, targetId))
+                throw new DataValidationException("The subscription has already been issued");
+        }
+        subscriptionRepository.followUser(followerId, targetId);
+    }
 }
