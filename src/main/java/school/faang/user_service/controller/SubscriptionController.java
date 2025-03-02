0d73e8a7df8a025bb39ffcb5de4d764c81659816
Artemon <artemon.kos@outlook.com>
2025-03-02 21:22:32 +0100

    fitch user subscription

--- src/main/java/school/faang/user_service/controller/SubscriptionController.java
+++ src/main/java/school/faang/user_service/controller/SubscriptionController.java
@@ -1,4 +1,18 @@
 package school.faang.user_service.controller;
 
+import jakarta.xml.bind.ValidationException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import school.faang.user_service.service.SubscriptionService;
+
+import java.util.Objects;
+
+@Controller
+@RequiredArgsConstructor
 public class SubscriptionController {
+    private SubscriptionService subscriptionService;
+
+    public void followUser(long followerId, long targetId) {
+        subscriptionService.followUser(followerId, targetId);
+    }
 }
