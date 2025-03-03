52a0b3a3ffaab415557ed34575b779edeb5718bc
Shakhzod Kamolov <shakhzod.kamolov99@gmail.com>
2025-03-04 02:29:13 +0500

    Feat(53206) Fix issues

--- src/main/java/school/faang/user_service/service/premium/PremiumBoughtEventHandler.java
+++ src/main/java/school/faang/user_service/service/premium/PremiumBoughtEventHandler.java
@@ -2,12 +2,12 @@ package school.faang.user_service.service.premium;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import school.faang.user_service.dto.PremiumBoughtEvent;
 import school.faang.user_service.mapper.RedisEventMapper;
-import school.faang.user_service.properties.UserServiceProperties;
 import school.faang.user_service.redis.RedisEventPublisher;
 import school.faang.user_service.redis.event.PremiumBoughtRedisEvent;
 
@@ -18,9 +18,9 @@ public class PremiumBoughtEventHandler {
 
     private final RedisEventPublisher premiumBoughtEventPublisher;
     private final RedisEventMapper redisEventMapper;
-    private final UserServiceProperties properties;
 
-    private final String topic = properties.getRedis().getChannel().getBoughtPremiumTopic();
+    @Value("${user-service.redis.bought-premium-topic}")
+    private String topic;
 
     @Async
     @EventListener
