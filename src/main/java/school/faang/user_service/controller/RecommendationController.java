ee927c2706786e7713bfb2528dee7892bf5c9f60
Yaroslav Olkin <shrelok.pupkin@yandex.ru>
2025-03-04 20:24:17 +0300

    --

--- src/main/java/school/faang/user_service/controller/RecommendationController.java
+++ src/main/java/school/faang/user_service/controller/RecommendationController.java
@@ -1,16 +1,18 @@
 package school.faang.user_service.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import school.faang.user_service.dto.recommendation.RecommendationDto;
+import school.faang.user_service.entity.recommendation.Recommendation;
 import school.faang.user_service.service.RecommendationService;
 
-
+@RequiredArgsConstructor
 @Controller
 public class RecommendationController {
     private final RecommendationService recommendationService;
 
-    @Autowired
-    public RecommendationController(RecommendationService recommendationService) {
-        this.recommendationService = recommendationService;
+    public void updateRecommendation(RecommendationDto recommendationDto) {
+        return ;
     }
 }
--- src/main/java/school/faang/user_service/dto/recommendation/RecommendationDto.java
+++ src/main/java/school/faang/user_service/dto/recommendation/RecommendationDto.java
@@ -1,8 +1,15 @@
 package school.faang.user_service.dto.recommendation;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
+@AllArgsConstructor
+@Getter
+@Setter
 public class RecommendationDto {
     private Long id;
     private Long authorId;
