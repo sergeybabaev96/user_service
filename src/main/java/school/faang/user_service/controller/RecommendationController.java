aae8af9fb3f2075f62cb0852f226b33a0dc3260a
Yaroslav Olkin <shrelok.pupkin@yandex.ru>
2025-03-04 22:51:59 +0300

    -

--- src/main/java/school/faang/user_service/controller/RecommendationController.java
+++ src/main/java/school/faang/user_service/controller/RecommendationController.java
@@ -1,10 +1,8 @@
 package school.faang.user_service.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import school.faang.user_service.dto.recommendation.RecommendationDto;
-import school.faang.user_service.entity.recommendation.Recommendation;
 import school.faang.user_service.service.RecommendationService;
 
 @RequiredArgsConstructor
@@ -12,7 +10,7 @@ import school.faang.user_service.service.RecommendationService;
 public class RecommendationController {
     private final RecommendationService recommendationService;
 
-    public void updateRecommendation(RecommendationDto recommendationDto) {
-        return ;
+    public RecommendationDto updateRecommendation(RecommendationDto recommendationDto) {
+//        return recommendationService.update(RecommendationDto recommendation);
     }
 }
