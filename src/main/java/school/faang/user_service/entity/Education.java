e261e6e2f969e1912ff6d0ff867a3b0cce6e6850
Alexti25 <alxej.kazakov@gmail.com>
2025-03-05 16:10:07 +0300

    BJS2-65927-Education

--- src/main/java/school/faang/user_service/entity/Education.java
+++ src/main/java/school/faang/user_service/entity/Education.java
@@ -8,12 +8,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
