2b94b1678dd1348463f593e7dcc1fddf4df10941
Mipusatina <mipspam@mail.ru>
2025-03-05 02:13:05 +0100

    GlobalControllerAdvice for other Exceptions

--- src/main/java/school/faang/user_service/controller/GlobalControllerAdvice.java
+++ src/main/java/school/faang/user_service/controller/GlobalControllerAdvice.java
@@ -1,15 +1,19 @@
 package school.faang.user_service.controller;
 
 import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalControllerAdvice {
 
@@ -20,6 +24,8 @@ public class GlobalControllerAdvice {
         e.getBindingResult()
                 .getFieldErrors()
                  .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+
+        logging(LoggingLevel.WARN, errors);
         return errors;
     }
 
@@ -30,6 +36,40 @@ public class GlobalControllerAdvice {
         e.getConstraintViolations()
                 .forEach(violation -> errors.
                         put(violation.getPropertyPath().toString(), violation.getMessage()));
+
+        logging(LoggingLevel.WARN, errors);
         return errors;
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public Map.Entry<String, String> otherExceptionHandler(Exception e) {
+        Map.Entry<String, String> error = Map.entry(e.getCause().toString(), e.getMessage());
+
+        logging(LoggingLevel.ERROR, error);
+        return error;
+    }
+
+
+    private void logging(LoggingLevel level, Map<String, String> errors) {
+        errors.entrySet().forEach(entry -> logging(level, entry));
+    }
+
+    private void logging(LoggingLevel level, Map.Entry<String, String> error) {
+        logging(level, String.format("%s: %s", error.getKey(), error.getValue()));
+    }
+
+    private void logging(LoggingLevel level, String errorMessage) {
+        level.command.accept(errorMessage);
+    }
+}
+
+@Slf4j
+enum LoggingLevel {
+    INFO(log::info), DEBUG(log::debug), WARN(log::warn), ERROR(log::error);
+
+    final Consumer<String> command;
+    LoggingLevel(Consumer<String> command) {
+        this.command = command;
     }
 }
