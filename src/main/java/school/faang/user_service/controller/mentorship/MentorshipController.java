efe432e5e355a6b2833dde84642dbfdd0d7dbcf9
Alex P <a117a117a@gmail.com>
2025-03-05 23:16:41 +0100

    replace RestController to Component in MentorshipController

--- src/main/java/school/faang/user_service/controller/mentorship/MentorshipController.java
+++ src/main/java/school/faang/user_service/controller/mentorship/MentorshipController.java
@@ -1,22 +1,19 @@
 package school.faang.user_service.controller.mentorship;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Component;
 import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.service.MentorshipService;
+
 import java.util.List;
 
-@RestController
-@RequestMapping("/mentorship")
+@Component
 @RequiredArgsConstructor
 public class MentorshipController {
 
     private final MentorshipService mentorshipService;
 
-    @GetMapping("/{mentorId}/mentees")
-    public ResponseEntity<List<UserDto>> getMentees(@PathVariable Long mentorId) {
-        List<UserDto> mentees = mentorshipService.getMentees(mentorId);
-        return ResponseEntity.ok(mentees);
+    public List<UserDto> getMentees(Long mentorId) {
+        return mentorshipService.getMentees(mentorId);
     }
 }
