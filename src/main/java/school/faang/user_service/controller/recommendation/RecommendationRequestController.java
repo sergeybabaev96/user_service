b939244bd84c4ed91d9c22038b6566af3858818e
Anton <basanton@mail.ru>
2025-03-05 11:50:29 +0300

    Скорректировал exception'ы. Поправил валидатор user'а v2

--- src/main/java/school/faang/user_service/controller/recommendation/RecommendationRequestController.java
+++ src/main/java/school/faang/user_service/controller/recommendation/RecommendationRequestController.java
@@ -1,7 +1,9 @@
 package school.faang.user_service.controller.recommendation;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +15,7 @@ import school.faang.user_service.service.recommendation.RecommendationRequestSer
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class RecommendationRequestController {
@@ -20,22 +23,26 @@ public class RecommendationRequestController {
 
     public RecommendationRequestDto requestRecommendation(
             @Valid @RequestBody RecommendationRequestDto recommendationRequest) {
+        log.info("Received request to create recommendation: {}", recommendationRequest);
         return recommendationRequestService.create(recommendationRequest);
     }
 
     public List<RecommendationRequestDto> getRecommendationRequests(
             @Valid @ModelAttribute RequestFilterDto filter) {
+        log.info("Received request to get recommendations with filter: {}", filter);
         return recommendationRequestService.getRequests(filter);
     }
 
     public RecommendationRequestDto getRecommendationRequest(
-            @PathVariable Long id) {
+            @PathVariable @Positive Long id) {
+        log.info("Received request to get recommendation with id: {}", id);
         return recommendationRequestService.getRequest(id);
     }
 
     public RecommendationRequestDto rejectRequest(
-            @PathVariable Long id,
+            @PathVariable @Positive Long id,
             @Valid @RequestBody RejectionDto rejection) {
+        log.info("Received request to reject recommendation with id: {}", id);
         return recommendationRequestService.rejectRequest(id, rejection);
     }
 
--- src/main/java/school/faang/user_service/validator/user/UserValidator.java
+++ src/main/java/school/faang/user_service/validator/user/UserValidator.java
@@ -12,10 +12,12 @@ import school.faang.user_service.repository.UserRepository;
 public class UserValidator {
     private final UserRepository userRepository;
 
-    public void validateUser(Long userId) {
-        userRepository.findById(userId).orElseThrow(() -> {
-            log.error("User with id {} not found", userId);
-            return new EntityNotFoundException(String.format("There isn't user with id = %d", userId));
-        });
+    public void validateUser(Long id) {
+        log.info("Validating user with id: {}", id);
+        if (!userRepository.existsById(id)) {
+            log.error("User with id {} not found", id);
+            throw new EntityNotFoundException("User not found");
+        }
+        log.info("User with id {} validated successfully", id);
     }
 }
