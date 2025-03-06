16778d3e7b5db2968844d90c598bcdb971c10295
Yaroslav Olkin <shrelok.pupkin@yandex.ru>
2025-03-06 12:25:45 +0300

    исправил импорты

--- src/main/java/school/faang/user_service/dto/recommendation/RecommendationDto.java
+++ src/main/java/school/faang/user_service/dto/recommendation/RecommendationDto.java
@@ -1,9 +1,6 @@
 package school.faang.user_service.dto.recommendation;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
