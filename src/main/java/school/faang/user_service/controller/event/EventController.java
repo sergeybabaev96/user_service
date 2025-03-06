46185e52a4275740e625928a3311bea85c86dbcc
RamilNazmiev192 <nazmiev633@gmail.com>
2025-03-06 12:22:10 +0300

    refactor: change Controller and add requestMappingPath

--- src/main/java/school/faang/user_service/controller/event/EventController.java
+++ src/main/java/school/faang/user_service/controller/event/EventController.java
@@ -1,7 +1,16 @@
 package school.faang.user_service.controller.event;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import school.faang.user_service.dto.event.EventDto;
 import school.faang.user_service.filter.EventFilterDto;
 import school.faang.user_service.service.event.EventService;
@@ -10,37 +19,46 @@ import school.faang.user_service.validator.EventDtoValidator;
 import java.util.List;
 
 @RestController
+@RequestMapping("/events")
 @RequiredArgsConstructor
 public class EventController {
     private final EventService eventService;
 
-    public EventDto create(EventDto event) {
+    @PostMapping
+    public ResponseEntity<EventDto> create(@RequestBody EventDto event) {
         EventDtoValidator.validate(event);
-        return eventService.create(event);
+        return ResponseEntity.ok(eventService.create(event));
     }
 
-    public EventDto getEvent(long eventId) {
-        return eventService.getEvent(eventId);
+    @GetMapping("/{eventId}")
+    public ResponseEntity<EventDto> getEvent(@PathVariable long eventId) {
+        return ResponseEntity.ok(eventService.getEvent(eventId));
     }
 
-    public List<EventDto> getEventsByFilter(EventFilterDto filter) {
-        return eventService.getEventsByFilter(filter);
+    @GetMapping("/filter")
+    public ResponseEntity<List<EventDto>> getEventsByFilter(@ModelAttribute EventFilterDto filter) {
+        return ResponseEntity.ok(eventService.getEventsByFilter(filter));
     }
 
-    public void deleteEvent(long eventId) {
+    @DeleteMapping("/{eventId}")
+    public ResponseEntity<Void> deleteEvent(@PathVariable long eventId) {
         eventService.deleteEvent(eventId);
+        return ResponseEntity.noContent().build();
     }
 
-    public EventDto updateEvent(EventDto event) {
+    @PutMapping("/{eventId}")
+    public ResponseEntity<EventDto> updateEvent(@RequestBody EventDto event) {
         EventDtoValidator.validate(event);
-        return eventService.updateEvent(event);
+        return ResponseEntity.ok(eventService.updateEvent(event));
     }
 
-    public List<EventDto> getOwnedEvents(long userId) {
-        return eventService.getOwnedEvents(userId);
+    @GetMapping("/owned/{userId}")
+    public ResponseEntity<List<EventDto>> getOwnedEvents(@PathVariable long userId) {
+        return ResponseEntity.ok(eventService.getOwnedEvents(userId));
     }
 
-    public List<EventDto> getParticipatedEvents(long userId) {
-        return eventService.getParticipatedEvents(userId);
+    @GetMapping("/participated/{userId}")
+    public ResponseEntity<List<EventDto>> getParticipatedEvents(@PathVariable long userId) {
+        return ResponseEntity.ok(eventService.getParticipatedEvents(userId));
     }
 }
