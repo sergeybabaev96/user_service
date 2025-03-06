193d9897b881bc63452b144f3e11d07c655d1fc3
Mipusatina <mipspam@mail.ru>
2025-03-06 02:18:53 +0100

    UserController

--- src/main/java/school/faang/user_service/controller/user/UserController.java
+++ src/main/java/school/faang/user_service/controller/user/UserController.java
@@ -1,8 +1,6 @@
 package school.faang.user_service.controller.user;
 
-import com.fasterxml.jackson.databind.MappingIterator;
-import com.fasterxml.jackson.dataformat.csv.CsvMapper;
-import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,12 +10,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import school.faang.user_service.dto.user.Person;
 import school.faang.user_service.dto.user.UserDto;
+import school.faang.user_service.mapper.PersonCsvMapper;
 import school.faang.user_service.service.UserService;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 
 @Slf4j
@@ -29,26 +26,17 @@ public class UserController {
 
 
     private final UserService userService;
+    private final PersonCsvMapper personCsvMapper;
 
-    @PostMapping
-    public List<UserDto> parseCsvToUsers(
+    @PostMapping("/csv")
+    @Operation(
+            summary = "Create Users from CSV file",
+            description = "It parses CSV file. CSV file has to have column like Person.class/PersonCsvMapper.class")
+    public List<UserDto> createUsersFromCsvFile(
             @NotNull(message = "Request has to have a CSV file with Persons") @RequestBody MultipartFile file)
             throws IOException {
 
-        CsvMapper csvMapper = new CsvMapper();
-        CsvSchema schema = csvMapper.schemaFor(Person.class).withHeader();
-
-        List<Person> persons;
-
-        try (InputStream inputStream = file.getInputStream()) {
-            MappingIterator<Person> iterator;
-            iterator = csvMapper
-                    .readerFor(Person.class)
-                    .with(schema)
-                    .readValues(inputStream);
-            persons = iterator.readAll();
-        }
-
-        return userService.saveUsers(persons);
+        return userService.saveUsers(
+                personCsvMapper.toPersons(file));
     }
 }
