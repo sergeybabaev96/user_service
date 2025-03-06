340df31f15c2ba02e1849273c2a021fd64847f1d
Pavel Chernobay <mr.chernobay1990@gmail.com>
2025-03-06 21:19:28 +0300

    Убрал лишнии логи и добавил endpoints

--- src/main/java/school/faang/user_service/validator/user/UserValidator.java
+++ src/main/java/school/faang/user_service/validator/user/UserValidator.java
@@ -2,17 +2,20 @@ package school.faang.user_service.validator.user;
 
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import school.faang.user_service.repository.UserRepository;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class UserValidator {
 
     private final UserRepository userRepository;
 
-    public void checkUserExistsById(Long id) {
-        if (!userRepository.existsById(id)) {
+    public void checkUserExistsById(Long userId) {
+        if (!userRepository.existsById(userId)) {
+            log.warn("User with id {} not found", userId);
             throw new EntityNotFoundException("User not found");
         }
     }
