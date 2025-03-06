c504329b3608207f4ffb5329b8b63d31f8b4a74a
SergeyErmoshenko <ermoshenko.work@gmail.com>
2025-03-06 22:26:57 +0300

    Подписаться на пользователя (#2613)
    * Добавлена подписка на пользователя

--- src/main/java/school/faang/user_service/controller/SubscriptionController.java
+++ src/main/java/school/faang/user_service/controller/SubscriptionController.java
@@ -11,6 +11,11 @@ import school.faang.user_service.service.SubscriptionService;
 public class SubscriptionController {
     private final SubscriptionService subscriptionService;
 
+    public void followUser(long followerId, long followeeId) {
+        log.info("Received request to follow user. FollowerId: {}, FolloweeId: {}", followerId, followeeId);
+        subscriptionService.followUser(followerId, followeeId);
+        log.info("User {} successfully followed user {}", followerId, followeeId);
+
     public void unfollowUser(long followerId, long followeeId) {
         log.info("Unfollow request received: Follower ID = {}, Followee ID = {}", followerId, followeeId);
         subscriptionService.unfollowUser(followerId, followeeId);
