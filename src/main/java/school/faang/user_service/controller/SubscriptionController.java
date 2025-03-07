63d996643637a3f0129f6fef29fe40d8daa87abf
vtroshin <vtroshin@rvision.ru>
2025-03-07 13:08:16 +0300

    BJS2-65886 Правки в контроллере

--- src/main/java/school/faang/user_service/controller/SubscriptionController.java
+++ src/main/java/school/faang/user_service/controller/SubscriptionController.java
@@ -1,13 +1,16 @@
 package school.faang.user_service.controller;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.websocket.server.PathParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.dto.UserFilterDto;
-import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.service.SubscriptionService;
 
 import java.util.List;
@@ -19,60 +22,32 @@ public class SubscriptionController {
     private final SubscriptionService subscriptionService;
 
     @PostMapping(value = "/follow")
-    public void followUser(long followerId, long followeeId) {
-        if (followerId <= 0 || followeeId <= 0) {
-            throw new DataValidationException("ID пользователей должны быть положительными.");
-        }
-        if (followerId == followeeId) {
-            throw new DataValidationException("Нельзя подписаться на самого себя.");
-        }
+    public void followUser(@NotNull @RequestParam Long followerId, @NotNull @RequestParam Long followeeId) {
         subscriptionService.followUser(followerId, followeeId);
     }
 
     @PostMapping(value = "/unfollow")
-    public void unfollowUser(long followerId, long followeeId) {
-        if (followerId <= 0 || followeeId <= 0) {
-            throw new DataValidationException("ID пользователей должны быть положительными.");
-        }
-        if (followerId == followeeId) {
-            throw new DataValidationException("Нельзя отписаться на самого себя.");
-        }
+    public void unfollowUser(@NotNull @RequestParam Long followerId, @NotNull @RequestParam Long followeeId) {
         subscriptionService.unfollowUser(followerId, followeeId);
     }
 
-    @GetMapping(value = "/get-following")
-    public List<UserDto> getFollowing(long followeeId, UserFilterDto filter) {
-        if (followeeId <= 0) {
-            throw new DataValidationException("ID Пользователя должен быть положительным");
-        }
-
+    @GetMapping(value = "/followers/my")
+    public List<UserDto> getFollowing(@NotNull @RequestParam Long followeeId, @RequestParam UserFilterDto filter) {
         return subscriptionService.getFollowing(followeeId, filter);
     }
 
-    @GetMapping(value = "/follow-count")
-    public int getFollowingCount(long followerId) {
-        if (followerId <= 0) {
-            throw new DataValidationException("ID Пользователя должен быть положительным");
-        }
-
+    @GetMapping(value = "/followers/my/count")
+    public int getFollowingCount(@NotNull @RequestParam Long followerId) {
         return subscriptionService.getFollowingCount(followerId);
     }
 
-    @GetMapping(value = "/get-followers")
-    public List<UserDto> getFollowers(long followeeId, UserFilterDto filter) {
-        if (followeeId <= 0) {
-            throw new DataValidationException("ID Пользователя должен быть положительным");
-        }
-
-        return subscriptionService.getFollowers(followeeId, filter);
+    @GetMapping(value = "/followers/users/{id}")
+    public List<UserDto> getFollowers(@NotNull @PathVariable("id") Long id, @NotNull @RequestParam UserFilterDto filter) {
+        return subscriptionService.getFollowers(id, filter);
     }
 
-    @GetMapping(value = "/followers-count")
-    public int getFollowersCount(long followeeId) {
-        if (followeeId <= 0) {
-            throw new DataValidationException("ID Пользователя должен быть положительным");
-        }
-
-        return subscriptionService.getFollowersCount(followeeId);
+    @GetMapping(value = "/followers/users/{id}/count")
+    public int getFollowersCount(@NotNull @PathVariable Long id) {
+        return subscriptionService.getFollowersCount(id);
     }
 }
