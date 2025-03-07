d2dc1ef1e4dc58b08a3cf4316111478ac37fa079
kaktus <dda20040609@gmail.com>
2025-03-07 12:32:34 +0300

    new fix commit

--- src/main/java/school/faang/user_service/messaging/kafka/RecommendationRequestedEventPublisher.java
+++ src/main/java/school/faang/user_service/messaging/kafka/RecommendationRequestedEventPublisher.java
@@ -1,19 +1,22 @@
 package school.faang.user_service.messaging.kafka;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import school.faang.user_service.dto.recommendation.RecommendationRequestedEvent;
-
 @Service
 public class RecommendationRequestedEventPublisher {
 
     private final KafkaTemplate<String, RecommendationRequestedEvent> kafkaTemplate;
+    private final String topic;
 
-    public RecommendationRequestedEventPublisher(KafkaTemplate<String, RecommendationRequestedEvent> kafkaTemplate) {
+    public RecommendationRequestedEventPublisher(KafkaTemplate<String, RecommendationRequestedEvent> kafkaTemplate,
+                                                 @Value("${spring.kafka.topics.recommendation_requests}") String topic) {
         this.kafkaTemplate = kafkaTemplate;
+        this.topic = topic;
     }
 
     public void publishEvent(RecommendationRequestedEvent event) {
-        kafkaTemplate.send("recommendation_requests", event);
+        kafkaTemplate.send(topic, event);
     }
 }
