31cb2321734c8a76fc314577da53150583460097
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-03-07 15:39:26 +0500

    https://faang-school.atlassian.net/browse/BJS2-65084
    correct controller

--- src/main/java/school/faang/user_service/controller/education/EducationController.java
+++ src/main/java/school/faang/user_service/controller/education/EducationController.java
@@ -1,27 +1,28 @@
 package school.faang.user_service.controller.education;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import school.faang.user_service.dto.EducationDto;
 import school.faang.user_service.service.education.EducationService;
 
-@Controller
+@RestController
+@RequestMapping("/education")
 @RequiredArgsConstructor
 public class EducationController {
     private final EducationService educationService;
 
-    @PostMapping
-    public EducationDto addEducation(long userId, EducationDto educationDto) {
+    @PostMapping("/{userId}")
+    public EducationDto addEducation(@PathVariable long userId, @RequestBody EducationDto educationDto) {
         return educationService.addEducation(userId, educationDto);
     }
 
-    @PostMapping
-    public EducationDto updateEducation(long userId, EducationDto educationDto) {
+    @PutMapping("/{userId}")
+    public EducationDto updateEducation(@PathVariable long userId, @RequestBody EducationDto educationDto) {
         return educationService.updateEducation(userId, educationDto);
     }
 
-    public EducationDto getById(long educationId) {
+    @GetMapping("/{educationId}")
+    public EducationDto getById(@PathVariable long educationId) {
         return educationService.getById(educationId);
     }
 }
