207c922d0cf3ec4271168f92090190c22cd7466d
vtroshin <vtroshin@rvision.ru>
2025-03-08 18:56:25 +0300

    BJS2-65886 Переименовал эндпоинты

--- src/main/java/school/faang/user_service/controller/SubscriptionController.java
+++ src/main/java/school/faang/user_service/controller/SubscriptionController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.FollowerResponse;
 import school.faang.user_service.dto.UserFilterRequest;
@@ -15,33 +14,35 @@ import school.faang.user_service.service.SubscriptionService;
 import java.util.List;
 
 @RestController
-@RequestMapping("/subscribe")
+@RequestMapping("/subscription")
 @RequiredArgsConstructor
 public class SubscriptionController {
     private final SubscriptionService subscriptionService;
 
-    @PostMapping(value = "/follow")
-    public void followUser(@NotNull @RequestParam Long followerId, @NotNull @RequestParam Long followeeId) {
+    @PostMapping(value = "{followerId}/follow/{followeeId}")
+    public void followUser(@NotNull @PathVariable("followerId") Long followerId,
+                           @NotNull @PathVariable("followeeId") Long followeeId) {
         subscriptionService.followUser(followerId, followeeId);
     }
 
-    @PostMapping(value = "/unfollow")
-    public void unfollowUser(@NotNull @RequestParam Long followerId, @NotNull @RequestParam Long followeeId) {
+    @PostMapping(value = "{followerId}/unfollow/{followeeId}")
+    public void unfollowUser(@NotNull @PathVariable("followerId") Long followerId,
+                             @NotNull @PathVariable("followeeId") Long followeeId) {
         subscriptionService.unfollowUser(followerId, followeeId);
     }
 
-    @GetMapping(value = "/followers/my")
-    public List<FollowerResponse> getFollowing(@NotNull @RequestParam Long followeeId, @RequestParam UserFilterRequest filter) {
-        return subscriptionService.getFollowing(followeeId, filter);
+    @GetMapping(value = "/followers/{id}")
+    public List<FollowerResponse> getFollowing(@NotNull @PathVariable("id") Long id, UserFilterRequest filter) {
+        return subscriptionService.getFollowing(id, filter);
     }
 
-    @GetMapping(value = "/followers/my/count")
-    public int getFollowingCount(@NotNull @RequestParam Long followerId) {
-        return subscriptionService.getFollowingCount(followerId);
+    @GetMapping(value = "/followers/{id}/count")
+    public int getFollowingCount(@NotNull @PathVariable("id") Long id) {
+        return subscriptionService.getFollowingCount(id);
     }
 
     @GetMapping(value = "/followers/users/{id}")
-    public List<FollowerResponse> getFollowers(@NotNull @PathVariable("id") Long id, @NotNull @RequestParam UserFilterRequest filter) {
+    public List<FollowerResponse> getFollowers(@NotNull @PathVariable("id") Long id, UserFilterRequest filter) {
         return subscriptionService.getFollowers(id, filter);
     }
 
