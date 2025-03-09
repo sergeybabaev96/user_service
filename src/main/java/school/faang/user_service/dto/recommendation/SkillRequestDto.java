b9960fd8e83bb1dbf8a791687703aed189b831c9
KAS1990 <34664788+KAS1990@users.noreply.github.com>
2025-03-09 20:53:19 +0300

    fix

--- src/main/java/school/faang/user_service/dto/recommendation/SkillRequestDto.java
+++ src/main/java/school/faang/user_service/dto/recommendation/SkillRequestDto.java
@@ -1,5 +1,8 @@
 package school.faang.user_service.dto.recommendation;
 
+import org.springframework.lang.Nullable;
+
 public record SkillRequestDto(
-        long skillId) {
+        long skillId,
+        @Nullable Long requestId) {
 }
