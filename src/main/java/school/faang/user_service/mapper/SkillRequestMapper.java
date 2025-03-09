83531e994a20b332503cf6c419bf86c30f824b6c
KAS1990 <34664788+KAS1990@users.noreply.github.com>
2025-03-09 21:17:38 +0300

    fix:

--- src/main/java/school/faang/user_service/mapper/SkillRequestMapper.java
+++ src/main/java/school/faang/user_service/mapper/SkillRequestMapper.java
@@ -2,16 +2,14 @@ package school.faang.user_service.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 import school.faang.user_service.entity.recommendation.SkillRequest;
 import school.faang.user_service.dto.recommendation.SkillRequestDto;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface SkillRequestMapper {
     @Mapping(target = "skillId", source = "skill.id")
-    @Mapping(target = "requestId", source = "request.id")
     SkillRequestDto toDto(SkillRequest entity);
 
-    @Mapping(target = "request", ignore = true)
-    @Mapping(target = "skill", ignore = true)
     SkillRequest toEntity(SkillRequestDto dto);
 }
