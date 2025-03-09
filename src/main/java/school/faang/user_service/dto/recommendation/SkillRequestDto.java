ef0c210917c3bc2c0d159a076bf0b023f853a35c
KAS1990 <34664788+KAS1990@users.noreply.github.com>
2025-03-09 21:06:21 +0300

    fix: Removed unneeded field

--- src/main/java/school/faang/user_service/dto/recommendation/SkillRequestDto.java
+++ src/main/java/school/faang/user_service/dto/recommendation/SkillRequestDto.java
@@ -1,8 +1,5 @@
 package school.faang.user_service.dto.recommendation;
 
-import org.springframework.lang.Nullable;
-
 public record SkillRequestDto(
-        long skillId,
-        @Nullable Long requestId) {
+        long skillId) {
 }
