d8abcb55cd960841833c74e9d36cb20348b0d485
KAS1990 <34664788+KAS1990@users.noreply.github.com>
2025-03-09 18:12:44 +0300

    refactor: Used unmappedTargetPolicy

--- src/main/java/school/faang/user_service/mapper/RecommendationRequestMapper.java
+++ src/main/java/school/faang/user_service/mapper/RecommendationRequestMapper.java
@@ -2,20 +2,15 @@ package school.faang.user_service.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 import school.faang.user_service.entity.recommendation.RecommendationRequest;
 import school.faang.user_service.repository.recommendation.RecommendationRequestDto;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface RecommendationRequestMapper {
-    @Mapping(target = "requester", ignore = true)
-    @Mapping(target = "receiver", ignore = true)
-    @Mapping(target = "recommendation", ignore = true)
-    @Mapping(target = "rejectionReason", ignore = true)
-    @Mapping(target = "skills", ignore = true)
     RecommendationRequest toEntity(RecommendationRequestDto dto);
 
     @Mapping(target = "requesterId", source = "requester.id")
     @Mapping(target = "receiverId", source = "receiver.id")
-    @Mapping(target = "skills", ignore = true)
     RecommendationRequestDto toDto(RecommendationRequest entity);
 }
