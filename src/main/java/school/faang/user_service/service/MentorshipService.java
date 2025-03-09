c078e214ee016e39242d5b82776638713cc8edfa
Andrey Ievlev <avievlev@gmail.com>
2025-03-09 19:36:49 +0300

    BJS2-65039 Become Mentor: new realisation task

--- src/main/java/school/faang/user_service/service/MentorshipService.java
+++ src/main/java/school/faang/user_service/service/MentorshipService.java
@@ -6,50 +6,51 @@ import org.springframework.stereotype.Service;
 import school.faang.user_service.dto.MentorshipDto;
 import school.faang.user_service.entity.User;
 import school.faang.user_service.mapper.MentorshipMapper;
-import school.faang.user_service.repository.mentorship.MentorshipRepository;
+import school.faang.user_service.repository.UserRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class MentorshipService {
-    private final MentorshipRepository mentorshipRepository;
+    private final UserRepository userRepository;
     private final MentorshipMapper mentorshipMapper;
 
     public List<MentorshipDto> getMentees(long userId) {
-        List<User> mentees = mentorshipRepository.findAllMenteesByUserId(userId);
-        return mentorshipMapper.toDtos(mentees);
+        return userRepository.findById(userId)
+                .map(User::getMentees)
+                .orElse(List.of())
+                .stream()
+                .map(mentorshipMapper::toDto)
+                .collect(Collectors.toList());
     }
 
     public List<MentorshipDto> getMentors(long userId) {
-        List<User> mentors = mentorshipRepository.findAllMentorsByUserId(userId);
-        return mentorshipMapper.toDtos(mentors);
+        return userRepository.findById(userId)
+                .map(user -> user.getMentors()
+                        .stream()
+                        .map(mentorshipMapper::toDto)
+                        .toList())
+                .orElseGet(List::of);
     }
 
     public void deleteMentee(long menteeId, long mentorId) {
-        User user = mentorshipRepository.findUserById(mentorId).orElseThrow(() -> new IllegalStateException("Mentor not found."));
-        List<User> mentees = mentorshipRepository.findAllMenteesByUserId(user.getId());
-        if (mentees.isEmpty()) {
-            throw new IllegalStateException("Mentor has not mentees");
-        }
-        boolean removed = user.getMentees().removeIf(mentee -> mentee.getId() == menteeId);
-        if (!removed) {
+        User mentor = userRepository.findById(mentorId)
+                .orElseThrow(() -> new IllegalStateException("Mentor not found."));
+        if (!mentor.getMentees().removeIf(mentee -> mentee.getId() == menteeId)) {
             throw new IllegalStateException("The mentee was not found in the mentor's list.");
         }
-        mentorshipRepository.save(user);
+        userRepository.save(mentor);
     }
 
     public void deleteMentor(long menteeId, long mentorId) {
-        User user = mentorshipRepository.findUserById(menteeId).orElseThrow(() -> new IllegalStateException("Mentee not found"));
-        List<User> mentors = mentorshipRepository.findAllMentorsByUserId(user.getId());
-        if (mentors.isEmpty()) {
-            throw new IllegalStateException("Mentee has not mentors");
-        }
-        boolean removed = user.getMentors().removeIf(mentor -> mentor.getId() == mentorId);
-        if (!removed) {
+        User mentee = userRepository.findById(menteeId)
+                .orElseThrow(() -> new IllegalStateException("Mentee not found"));
+        if (!mentee.getMentors().removeIf(mentor -> mentor.getId() == mentorId)) {
             throw new IllegalStateException("The mentor was not found in the mentee's list.");
         }
-        mentorshipRepository.save(user);
+        userRepository.save(mentee);
     }
 }
