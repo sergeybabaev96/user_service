9b6f7d1ddd231b72e9076fb563895899c2c56a43
Kirill Pankov <uralostrov6@gmail.com>
2025-03-09 17:07:25 +0500

    написал метод получение всех рекомендаций у пользователя использую тут Page

--- src/main/java/school/faang/user_service/repository/recommendation/RecommendationRepository.java
+++ src/main/java/school/faang/user_service/repository/recommendation/RecommendationRepository.java
@@ -2,14 +2,14 @@ package school.faang.user_service.repository.recommendation;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import school.faang.user_service.entity.recommendation.Recommendation;
 
 import java.util.Optional;
 
-public interface RecommendationRepository extends CrudRepository<Recommendation, Long> {
+public interface RecommendationRepository extends JpaRepository<Recommendation, Long> {
 
     @Query(nativeQuery = true, value = """
             INSERT INTO recommendation (author_id, receiver_id, content)
