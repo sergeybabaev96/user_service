7f11d89bad5c3bbdea180958c6cd9aa92b9af88b
SergeyErmoshenko <ermoshenko.work@gmail.com>
2025-03-09 17:59:06 +0300

    Получить всех подписчиков | Получить количество подписчиков (#2616)
    * добавлено получение всех подпичиков и кол-ва подписчиков
    
    * исправил ошибку mapper
    
    * добавлены логи, исправлена фильтрация подписчиков

--- src/main/java/school/faang/user_service/controller/SubscriptionController.java
+++ src/main/java/school/faang/user_service/controller/SubscriptionController.java
@@ -3,13 +3,31 @@ package school.faang.user_service.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import school.faang.user_service.dto.UserDto;
+import school.faang.user_service.dto.UserFilterDto;
 import school.faang.user_service.service.SubscriptionService;
 
-@RequiredArgsConstructor
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 @Slf4j
 public class SubscriptionController {
     private final SubscriptionService subscriptionService;
+
+    public List<UserDto> getFollowers(long followeeId, UserFilterDto userFilterDto) {
+        log.info("Getting followers for followeeId: {} with filter: {}", followeeId, userFilterDto);
+        List<UserDto> followers = subscriptionService.getFollowers(followeeId, userFilterDto);
+        log.info("Retrieved {} followers for followeeId: {}", followers.size(), followeeId);
+        return followers;
+    }
+
+    public int getFollowersCount(long followeeId) {
+        log.info("Getting followers count for followeeId: {}", followeeId);
+        int followersCount = subscriptionService.getFollowersCount(followeeId);
+        log.info("Retrieved followers count: {} for followeeId: {}", followersCount, followeeId);
+        return followersCount;
+    }
 
     public void followUser(long followerId, long followeeId) {
         log.info("Received request to follow user. FollowerId: {}, FolloweeId: {}", followerId, followeeId);
@@ -22,4 +40,6 @@ public class SubscriptionController {
         subscriptionService.unfollowUser(followerId, followeeId);
         log.info("Successfully unfollowed: Follower ID = {}, Followee ID = {}", followerId, followeeId);
     }
+
+
 }
