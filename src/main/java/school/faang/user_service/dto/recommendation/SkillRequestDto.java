1bffa87a643644f544e815a119765d1672396735
KAS1990 <34664788+KAS1990@users.noreply.github.com>
2025-03-09 20:51:38 +0300

    refactor: Removed unneeded fields

--- src/main/java/school/faang/user_service/dto/recommendation/SkillRequestDto.java
+++ src/main/java/school/faang/user_service/dto/recommendation/SkillRequestDto.java
@@ -1,7 +1,5 @@
 package school.faang.user_service.dto.recommendation;
 
 public record SkillRequestDto(
-        long id,
-        long skillId,
-        long requestId) {
+        long skillId) {
 }
