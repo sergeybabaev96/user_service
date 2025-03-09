5951f67f8725c47a9360354887561e600561460a
Andrey Ievlev <avievlev@gmail.com>
2025-03-09 19:36:49 +0300

    BJS2-65039 Become Mentor: new realisation task

--- src/main/java/school/faang/user_service/mapper/MentorshipMapper.java
+++ src/main/java/school/faang/user_service/mapper/MentorshipMapper.java
@@ -2,12 +2,13 @@ package school.faang.user_service.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 import school.faang.user_service.dto.MentorshipDto;
 import school.faang.user_service.entity.User;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface MentorshipMapper {
 
     @Mapping(target = "userId", source = "entity.id")
@@ -16,38 +17,6 @@ public interface MentorshipMapper {
 
     @Mapping(target = "id", source = "dto.userId")
     @Mapping(target = "username", source = "dto.userName")
-    @Mapping(target = "email", ignore = true) // Игнорируем поля, которых нет в DTO
-    @Mapping(target = "phone", ignore = true)
-    @Mapping(target = "password", ignore = true)
-    @Mapping(target = "active", ignore = true)
-    @Mapping(target = "aboutMe", ignore = true)
-    @Mapping(target = "country", ignore = true)
-    @Mapping(target = "city", ignore = true)
-    @Mapping(target = "experience", ignore = true)
-    @Mapping(target = "createdAt", ignore = true)
-    @Mapping(target = "updatedAt", ignore = true)
-    @Mapping(target = "followers", ignore = true)
-    @Mapping(target = "followees", ignore = true)
-    @Mapping(target = "ownedEvents", ignore = true)
-    @Mapping(target = "mentees", ignore = true)
-    @Mapping(target = "mentors", ignore = true)
-    @Mapping(target = "receivedMentorshipRequests", ignore = true)
-    @Mapping(target = "sentMentorshipRequests", ignore = true)
-    @Mapping(target = "sentGoalInvitations", ignore = true)
-    @Mapping(target = "receivedGoalInvitations", ignore = true)
-    @Mapping(target = "goals", ignore = true)
-    @Mapping(target = "skills", ignore = true)
-    @Mapping(target = "participatedEvents", ignore = true)
-    @Mapping(target = "recommendationsGiven", ignore = true)
-    @Mapping(target = "recommendationsReceived", ignore = true)
-    @Mapping(target = "contacts", ignore = true)
-    @Mapping(target = "ratings", ignore = true)
-    @Mapping(target = "userProfilePic", ignore = true)
-    @Mapping(target = "contactPreference", ignore = true)
-    @Mapping(target = "premium", ignore = true)
-    @Mapping(target = "education", ignore = true)
-    @Mapping(target = "career", ignore = true)
-    @Mapping(target = "workSchedule", ignore = true)
     User toEntity(MentorshipDto dto);
 
     List<MentorshipDto> toDtos(List<User> entities);
--- src/main/java/school/faang/user_service/repository/mentorship/MentorshipRepository.java
+++ src/main/java/school/faang/user_service/repository/mentorship/MentorshipRepository.java
@@ -1,27 +1,9 @@
 package school.faang.user_service.repository.mentorship;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import school.faang.user_service.entity.User;
 
-import java.util.List;
-import java.util.Optional;
-
+@Repository
 public interface MentorshipRepository extends CrudRepository<User, Long> {
-
-    @Query(nativeQuery = true, value = """
-            SELECT m.* FROM mentorship m
-            JOIN users u ON u.id = m.mentor_id
-            WHERE m.mentor_id = :userId
-            """)
-    List<User> findAllMenteesByUserId(long userId);
-
-    @Query(nativeQuery = true, value = """
-            SELECT m.* FROM mentorship m
-            JOIN users u ON u.id = m.mentee_id
-            WHERE u.mentee_id = :userId
-            """)
-    List<User> findAllMentorsByUserId(long userId);
-
-    Optional<User> findUserById(long id);
 }
