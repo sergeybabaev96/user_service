a577cd7a86bbf64deb8f1ddd416050315b378589
Garib_Aghayev <garibaghayev@gmail.com>
2025-03-10 02:12:13 +0300

    Добавил Swagger для двух контроллеров с эндпоинтами: UserController и GoalInvitationController

--- src/main/java/school/faang/user_service/dto/user/UserDto.java
+++ src/main/java/school/faang/user_service/dto/user/UserDto.java
@@ -1,21 +1,24 @@
 package school.faang.user_service.dto.user;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 import lombok.NonNull;
-import school.faang.user_service.entity.contact.PreferredContact;
 
 @Builder
+@Schema(description = "DTO пользователя")
 public record UserDto(
         @NonNull
+        @Schema(description = "ID пользователя", example = "1")
         Long id,
         @NotEmpty(message = "Имя не может быть пустым")
         @Max(value = 64, message = "Имя не должно быть длиннее 64 символов")
+        @Schema(description = "Имя пользователя", example = "JohnDoe")
         String username,
         @Email
-        String email,
-        PreferredContact preference
+        @Schema(description = "Email пользователя", example = "johndoe@example.com")
+        String email
 ) {
 }
