c1464bcf27ae3c5ff4ddaddd0728cdff6942e7ce
Kirill Pankov <uralostrov6@gmail.com>
2025-03-09 21:33:12 +0500

    написал restAPI на RecommendationController

--- src/main/java/school/faang/user_service/controller/RecommendationController.java
+++ src/main/java/school/faang/user_service/controller/RecommendationController.java
@@ -4,7 +4,12 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.recommendation.RecommendationDto;
@@ -17,30 +22,35 @@ import java.util.List;
 public class RecommendationController {
     private final RecommendationService recommendationService;
 
-    public RecommendationDto giveRecommendation(@Valid RecommendationDto recommendation) {
+    @PostMapping("/recommendation/create")
+    public RecommendationDto giveRecommendation(@RequestBody @Valid RecommendationDto recommendation) {
         return recommendationService.create(recommendation);
     }
 
-    public RecommendationDto updateRecommendation(@Valid RecommendationDto updated) {
+    @PutMapping("/recommendation/update")
+    public RecommendationDto updateRecommendation(@RequestBody @Valid RecommendationDto updated) {
         return recommendationService.update(updated);
     }
 
-    public boolean deleteRecommendation(@PathVariable @Min(1) long id) {
-        return recommendationService.delete(id);
+    @DeleteMapping("/recommendation/{recommendationId}/delete")
+    public boolean deleteRecommendation(@PathVariable @Min(1) long recommendationId) {
+        return recommendationService.delete(recommendationId);
     }
 
+    @GetMapping("/recommendation/{receiverId}/getAllUserRecommendations")
     public Page<RecommendationDto> getAllUserRecommendations(
-            @PathVariable @Min(1) long recieverId,
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size
+            @PathVariable @Min(1) long receiverId,
+            @RequestParam(defaultValue = "0") @Min(0) int page,
+            @RequestParam(defaultValue = "5") @Min(5) int size
     ) {
-        return recommendationService.getAllUserRecommendations(recieverId, page, size);
+        return recommendationService.getAllUserRecommendations(receiverId, page, size);
     }
 
+    @GetMapping("/recommendation/{authorId}/getAllGivenRecommendations")
     public Page<RecommendationDto> getAllGivenRecommendations(
             @PathVariable @Min(1) long authorId,
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size) {
+            @RequestParam(defaultValue = "0") @Min(0) int page,
+            @RequestParam(defaultValue = "5") @Min(5) int size) {
 
         return recommendationService.getAllGivenRecommendations(authorId, page, size);
     }
