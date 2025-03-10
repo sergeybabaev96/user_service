440dea873cfd51ff79e56d595514c6ef2539a753
elsledkov <elsledkov@gmail.com>
2025-03-10 21:25:50 +0300

    Refactor: Add interface EventParticipationService, task BJS2-65898

--- src/main/java/school/faang/user_service/exception/EventExistException.java
+++ src/main/java/school/faang/user_service/exception/EventExistException.java
@@ -1,6 +1,6 @@
 package school.faang.user_service.exception;
 
-public class EventExistException extends RuntimeException{
+public class EventExistException extends RuntimeException {
 
     public EventExistException(String message) {
         super(message);
