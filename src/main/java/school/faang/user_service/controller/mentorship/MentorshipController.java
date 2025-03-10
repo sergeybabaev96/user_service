155a3f77e8636763d3e4cf34eab3623fcc1225f5
Andrey Ievlev <avievlev@gmail.com>
2025-03-10 23:07:52 +0300

    BJS2-65039 Become Mentor: bug fixes after Alexey's code-review

--- src/main/java/school/faang/user_service/controller/mentorship/MentorshipController.java
+++ src/main/java/school/faang/user_service/controller/mentorship/MentorshipController.java
@@ -1,10 +1,12 @@
 package school.faang.user_service.controller.mentorship;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.MentorshipDto;
 import school.faang.user_service.service.MentorshipService;
@@ -17,23 +19,27 @@ import java.util.List;
 public class MentorshipController {
     private final MentorshipService mentorshipService;
 
-    @GetMapping("/mentors/{userId}/mentees")
-    public List<MentorshipDto> getMentees(@PathVariable long userId) {
-        return mentorshipService.getMentees(userId);
+    @GetMapping("/mentors/{mentorId}/mentees")
+    @ResponseStatus(HttpStatus.OK)
+    public List<MentorshipDto> getMentees(@PathVariable("mentorId") long mentorId) {
+        return mentorshipService.getMentees(mentorId);
     }
 
-    @GetMapping("/mentees/{userId}/mentors")
-    public List<MentorshipDto> getMentors(@PathVariable long userId) {
-        return mentorshipService.getMentors(userId);
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/mentees/{menteeId}/mentors")
+    public List<MentorshipDto> getMentors(@PathVariable("menteeId") long menteeId) {
+        return mentorshipService.getMentors(menteeId);
     }
 
     @DeleteMapping("/mentors/{mentorId}/mentees/{menteeId}")
-    public void deleteMentee(@PathVariable long menteeId, @PathVariable long mentorId) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteMentee(@PathVariable("menteeId") long menteeId, @PathVariable("mentorId") long mentorId) {
         mentorshipService.deleteMentee(menteeId, mentorId);
     }
 
     @DeleteMapping("/mentees/{menteeId}/mentors/{mentorId}")
-    public void deleteMentor(@PathVariable long menteeId, @PathVariable long mentorId) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteMentor(@PathVariable("menteeId") long menteeId, @PathVariable("mentorId") long mentorId) {
         mentorshipService.deleteMentor(menteeId, mentorId);
     }
 }
--- src/main/java/school/faang/user_service/repository/UserRepository.java
+++ src/main/java/school/faang/user_service/repository/UserRepository.java
@@ -2,12 +2,14 @@ package school.faang.user_service.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import school.faang.user_service.entity.User;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query(nativeQuery = true, value = """
