26e33581507c518544c6691bb75c3775de550144
saroyangor <dr.saroyan98@gmail.com>
2025-03-10 21:00:58 +0300

    delete logger from controller

--- src/main/java/school/faang/user_service/controller/mentorship/MentorshipRequestController.java
+++ src/main/java/school/faang/user_service/controller/mentorship/MentorshipRequestController.java
@@ -1,7 +1,6 @@
 package school.faang.user_service.controller.mentorship;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +15,6 @@ import school.faang.user_service.service.MentorshipRequestService;
 
 import java.util.List;
 
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/mentorship")
