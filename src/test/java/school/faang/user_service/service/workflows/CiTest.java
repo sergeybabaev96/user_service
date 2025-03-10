c62478badf3a9c15f294f41f3e5f650ac3ffe589
Ilhan Mustafa <ilhnmst@gmail.com>
2025-03-10 14:56:36 +0300

    Make CiTest true

--- src/test/java/school/faang/user_service/service/workflows/CiTest.java
+++ src/test/java/school/faang/user_service/service/workflows/CiTest.java
@@ -1,6 +1,5 @@
 package school.faang.user_service.service.workflows;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -9,6 +8,6 @@ public class CiTest {
 
     @Test
     public void SomeTest() {
-        assertTrue(false);
+        assertTrue(true);
     }
 }
