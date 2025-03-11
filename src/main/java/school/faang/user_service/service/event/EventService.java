5b6eb2be3e4b8074a8b953b1157c97e1bd5f03fa
RamilNazmiev192 <nazmiev633@gmail.com>
2025-03-11 17:41:27 +0300

    refactor:EventService and EventDtoValidator

--- src/main/java/school/faang/user_service/service/event/EventService.java
+++ src/main/java/school/faang/user_service/service/event/EventService.java
@@ -4,18 +4,19 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import school.faang.user_service.dto.event.EventDto;
+import school.faang.user_service.entity.Skill;
 import school.faang.user_service.entity.User;
 import school.faang.user_service.entity.event.Event;
 import school.faang.user_service.exception.DataValidationException;
-import school.faang.user_service.filter.EventFilterDto;
+import school.faang.user_service.filter.Event.*;
 import school.faang.user_service.mapper.EventMapper;
 import school.faang.user_service.repository.SkillRepository;
 import school.faang.user_service.repository.UserRepository;
 import school.faang.user_service.repository.event.EventRepository;
-import school.faang.user_service.validator.EventDtoValidator;
 
-import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Component
@@ -27,11 +28,20 @@ public class EventService {
     private final SkillRepository skillRepository;
     private final EventMapper eventMapper;
 
-    private static final String EVENT_NOT_FOUND_LOG = "Event with id {} not found. Requested at: {} in method: {}";
-    private static final String EVENT_NOT_FOUND_EXCEPTION = "Event with id %s not found";
+    private static final String EVENT_NOT_FOUND_LOG_DETAIL = "Event with id {} not found";
+    private static final String EVENT_NOT_FOUND_LOG = "Event with id";
+
+    private static final String ERROR_USER_NOT_FOUND_DETAIL = "User with id {} not found";
+    private static final String ERROR_USER_NOT_FOUND = "User not found";
+
+    private static final String ERROR_SKILLS_NOT_FOUND_DETAIL = "User with id {} does not have the required skills for this event";
+    private static final String ERROR_SKILLS_NOT_FOUND = "User does not have the required skills for this event";
+
+    private static final String EVENT_ID_MISMATCH_DETAIL = "ID in request path {} does not match ID in request body {}";
+    private static final String EVENT_ID_MISMATCH = "ID in request path does not match ID in request body";
 
     public EventDto create(EventDto eventDto) {
-        User owner = EventDtoValidator.validateOwnerAndSkills(eventDto, userRepository);
+        User owner = validateOwnerAndSkills(eventDto);
         Event entity = eventMapper.toEntity(eventDto);
         entity.setOwner(owner);
         entity = eventRepository.save(entity);
@@ -40,12 +50,12 @@ public class EventService {
     }
 
     public EventDto getEvent(long eventId) {
-        Event event = eventRepository.findById(eventId)
+        return eventRepository.findById(eventId)
+                .map(eventMapper::toDto)
                 .orElseThrow(() -> {
-                    log.error(EVENT_NOT_FOUND_LOG, eventId, LocalDateTime.now(), "getEvent");
-                    return new DataValidationException(String.format(EVENT_NOT_FOUND_EXCEPTION, eventId));
+                    log.error(EVENT_NOT_FOUND_LOG_DETAIL, eventId);
+                    return new DataValidationException(String.format(EVENT_NOT_FOUND_LOG));
                 });
-        return eventMapper.toDto(event);
     }
 
     public List<EventDto> getEventsByFilter(EventFilterDto filterDto) {
@@ -62,25 +72,16 @@ public class EventService {
     }
 
     public void deleteEvent(long eventId) {
-        Event event = eventRepository.findById(eventId)
-                .orElseThrow(() -> {
-                    log.error(EVENT_NOT_FOUND_LOG, eventId, LocalDateTime.now(), "deleteEvent");
-                    return new DataValidationException(String.format(EVENT_NOT_FOUND_EXCEPTION, eventId));
-                });
-        eventRepository.delete(event);
+        validateEventID(eventId);
+        eventRepository.deleteById(eventId);
     }
 
-    public EventDto updateEvent(EventDto eventDto) {
-        Event existingEvent = eventRepository.findById(eventDto.getId())
-                .orElseThrow(() -> {
-                    log.error(EVENT_NOT_FOUND_LOG, eventDto.getId(), LocalDateTime.now(), "updateEvent");
-                    return new DataValidationException(
-                            String.format(EVENT_NOT_FOUND_EXCEPTION, eventDto.getId()));
-                        }
-                );
+    public EventDto updateEvent(EventDto eventDto, long eventId) {
+        validateEventID(eventId);
+        validateMismatched(eventId);
+        validateOwnerAndSkills(eventDto);
 
-        EventDtoValidator.validateOwnerAndSkills(eventDto, userRepository);
-        existingEvent.setRelatedSkills(EventDtoValidator.validateAndGetRelatedSkills(eventDto, skillRepository));
+        Event existingEvent = eventRepository.getReferenceById(eventId);
 
         eventMapper.updateEventFormDto(eventDto, existingEvent);
         existingEvent = eventRepository.save(existingEvent);
@@ -102,5 +103,40 @@ public class EventService {
         return events.stream()
                 .map(eventMapper::toDto)
                 .collect(Collectors.toList());
+    }
+
+    private User validateOwnerAndSkills(EventDto eventDto) {
+        User owner = userRepository.findById(eventDto.getOwnerId())
+                .orElseThrow(() -> {
+                    log.error(ERROR_USER_NOT_FOUND_DETAIL, eventDto.getOwnerId());
+                    return new DataValidationException(ERROR_USER_NOT_FOUND);
+                });
+
+        Set<Long> userSkillIds = owner.getSkills().stream()
+                .map(Skill::getId)
+                .collect(Collectors.toSet());
+
+        Set<Long> requiredSkillIds = Set.copyOf(eventDto.getRelatedSkills());
+
+        if (!userSkillIds.containsAll(requiredSkillIds)) {
+            log.error(ERROR_SKILLS_NOT_FOUND_DETAIL, eventDto.getOwnerId());
+            throw new DataValidationException(ERROR_SKILLS_NOT_FOUND);
+        }
+
+        return owner;
+    }
+
+    private void validateEventID(long eventId){
+        if(!eventRepository.existsById(eventId)){
+            log.error(EVENT_NOT_FOUND_LOG_DETAIL, eventId);
+            throw new DataValidationException(EVENT_NOT_FOUND_LOG);
+        }
+    }
+
+    private void validateMismatched(long eventId){
+        if(!eventRepository.existsById(eventId)){
+            log.error(EVENT_ID_MISMATCH_DETAIL, eventId);
+            throw new DataValidationException(EVENT_ID_MISMATCH);
+        }
     }
 }
--- src/main/java/school/faang/user_service/validator/EventDtoValidator.java
+++ src/main/java/school/faang/user_service/validator/EventDtoValidator.java
@@ -1,99 +1,43 @@
 package school.faang.user_service.validator;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 import school.faang.user_service.dto.event.EventDto;
-import school.faang.user_service.entity.Skill;
-import school.faang.user_service.entity.User;
 import school.faang.user_service.exception.DataValidationException;
-import school.faang.user_service.repository.SkillRepository;
-import school.faang.user_service.repository.UserRepository;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Slf4j
+@Component
 public class EventDtoValidator {
     private static final String ERROR_TITLE_NULL_OR_EMPTY = "Validation failed: Title can't be null or empty";
     private static final String ERROR_START_DATE_INVALID = "Validation failed: StartDate can't be null or in the past";
     private static final String ERROR_OWNER_ID_NULL = "Validation failed: OwnerId can't be null";
     private static final String ERROR_EVENT_NULL = "Validation failed: EventDto is null";
 
-    private static final String ERROR_USER_NOT_FOUND = "User with id {} not found. Requested at: {}";
-    private static final String ERROR_SKILLS_NOT_FOUND = "User with id {} does not have the required skills for this event. Requested at: {}";
-    private static final String USER_NOT_FOUND_EXCEPTION = "User with id %s not found";
-    private static final String USER_NOT_HAVE_SKILLS = "User with id %s does not have skills";
-
-    private static final String USER_HAVE_EXIST_SKILLS = "Some skills do not exist.";
-    private static final String ERROR_MISSING_SKILLS = "Some skills do not exist. Missing: {}. Requested at: {}";
-
 
     public static void validate(EventDto eventDto) {
         LocalDateTime currentTime = LocalDateTime.now();
 
         if (eventDto == null) {
-            log.error("{} | {}", currentTime, ERROR_EVENT_NULL);
+            log.error(ERROR_EVENT_NULL);
             throw new DataValidationException("EventDto can't be null.");
         }
 
         if (eventDto.getTitle() == null || eventDto.getTitle().isBlank()) {
-            log.error("{} | {}", currentTime, ERROR_TITLE_NULL_OR_EMPTY);
+            log.error(ERROR_TITLE_NULL_OR_EMPTY);
             throw new DataValidationException(ERROR_TITLE_NULL_OR_EMPTY);
         }
+
         if (eventDto.getStartDate() == null || eventDto.getStartDate().isBefore(currentTime)) {
-            log.error("{} | {}", currentTime, ERROR_START_DATE_INVALID);
+            log.error(ERROR_START_DATE_INVALID);
             throw new DataValidationException(ERROR_START_DATE_INVALID);
         }
+
         if (eventDto.getOwnerId() == null) {
-            log.error("{} | {}", currentTime, ERROR_OWNER_ID_NULL);
+            log.error(ERROR_OWNER_ID_NULL);
             throw new DataValidationException(ERROR_OWNER_ID_NULL);
         }
     }
 
-    public static User validateOwnerAndSkills(EventDto eventDto, UserRepository userRepository) {
-        LocalDateTime currentTime = LocalDateTime.now();
-
-        User owner = userRepository.findById(eventDto.getOwnerId())
-                .orElseThrow(() -> {
-                            log.error(ERROR_USER_NOT_FOUND, eventDto.getOwnerId(), currentTime);
-                            return new DataValidationException(String.format(USER_NOT_FOUND_EXCEPTION, eventDto.getOwnerId()));
-                        }
-                );
-
-        Set<Long> userSkillIds = owner.getSkills().stream()
-                .map(Skill::getId)
-                .collect(Collectors.toSet());
-
-        Set<Long> requiredSkillIds = Set.copyOf(eventDto.getRelatedSkills());
-
-        if (!userSkillIds.containsAll(requiredSkillIds)) {
-            log.error(ERROR_SKILLS_NOT_FOUND, eventDto.getOwnerId(), currentTime);
-            throw new DataValidationException(String.format(USER_NOT_HAVE_SKILLS, eventDto.getOwnerId()));
-        }
-
-        return owner;
-    }
-
-    public static List<Skill> validateAndGetRelatedSkills(EventDto eventDto, SkillRepository skillRepository) {
-        LocalDateTime currentTime = LocalDateTime.now();
-        if (eventDto.getRelatedSkills() == null || eventDto.getRelatedSkills().isEmpty()) {
-            log.info("Validation skipped: No skills required for event {}. Timestamp: {}", eventDto.getId(), currentTime);
-            return List.of();
-        }
-
-        List<Skill> skills = skillRepository.findAllById(eventDto.getRelatedSkills());
-
-        Set<Long> foundSkillIds = skills.stream().map(Skill::getId).collect(Collectors.toSet());
-        Set<Long> missingSkills = eventDto.getRelatedSkills().stream()
-                .filter(id -> !foundSkillIds.contains(id))
-                .collect(Collectors.toSet());
-
-        if (skills.size() != eventDto.getRelatedSkills().size()) {
-            log.error(ERROR_MISSING_SKILLS, missingSkills, currentTime);
-            throw new DataValidationException(USER_HAVE_EXIST_SKILLS);
-        }
-
-        return skills;
-    }
 }
