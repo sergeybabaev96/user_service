a63569412982bc892cbf9697496570f81fb3a7ca
Anton <basanton@mail.ru>
2025-03-11 10:36:31 +0300

    Настроил декоратор согласно примеру v3

--- src/main/java/school/faang/user_service/mapper/recommendation/RecommendationRequestMapper.java
+++ src/main/java/school/faang/user_service/mapper/recommendation/RecommendationRequestMapper.java
@@ -1,23 +1,25 @@
 package school.faang.user_service.mapper.recommendation;
 
+
+import org.mapstruct.DecoratedWith;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 import school.faang.user_service.dto.recommendation.RecommendationRequestDto;
 import school.faang.user_service.entity.recommendation.RecommendationRequest;
 import school.faang.user_service.entity.recommendation.SkillRequest;
-import school.faang.user_service.service.recommendation.SkillMapperService;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring", uses = {SkillMapperService.class})
+@Mapper(componentModel = "spring")
+@DecoratedWith(RecommendationRequestMapperDecorator.class)
 public interface RecommendationRequestMapper {
     @Mapping(target = "skillsIds", source = "skills", qualifiedByName = "mapSkillsToIds")
     @Mapping(target = "requesterId", source = "requester.id")
     @Mapping(target = "receiverId", source = "receiver.id")
     RecommendationRequestDto toDto(RecommendationRequest recommendationRequest);
 
-    @Mapping(target = "skills", source = "skillsIds")
+    @Mapping(target = "skills", source = "skillsIds", qualifiedByName = "mapIdsToSkills")
     @Mapping(target = "requester.id", source = "requesterId")
     @Mapping(target = "receiver.id", source = "receiverId")
     @Mapping(target = "rejectionReason", ignore = true)
@@ -25,10 +27,15 @@ public interface RecommendationRequestMapper {
     RecommendationRequest toEntity(RecommendationRequestDto recommendationRequestDto);
 
     @Named("mapSkillsToIds")
-    static List<Long> mapSkillsToIds(List<SkillRequest> skillRequests) {
+    default List<Long> mapSkillsToIds(List<SkillRequest> skillRequests) {
         if (skillRequests == null) {
             return List.of();
         }
         return skillRequests.stream().map(SkillRequest::getId).toList();
+    }
+
+    @Named("mapIdsToSkills")
+    default List<SkillRequest> mapIdsToSkills(List<Long> skillIds) {
+        return List.of();
     }
 }
