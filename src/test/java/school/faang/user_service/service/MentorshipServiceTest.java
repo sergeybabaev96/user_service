7ac1b4ea339ddb52ff08489650a73f59ac02eee9
update <filipp_Chudaev@mail.ru>
2025-03-11 09:47:50 +0500

    поправил замечания коллег!

---
+++ src/test/java/school/faang/user_service/service/MentorshipServiceTest.java
@@ -0,0 +1,23 @@
+package school.faang.user_service.service;
+
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import school.faang.user_service.repository.UserRepository;
+import school.faang.user_service.repository.mentorship.MentorshipRepository;
+
+public class MentorshipServiceTest {
+
+    @Mock
+    private MentorshipRepository mentorshipRepository;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private MentorshipService mentorshipService;
+
+    @Test
+    public void testPositive
+
+}
