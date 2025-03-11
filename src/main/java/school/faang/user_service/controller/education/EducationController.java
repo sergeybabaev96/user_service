dec4e728bed2419c20eb69688768dd897e04ffb5
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-03-11 17:06:15 +0500

    https://faang-school.atlassian.net/browse/BJS2-65084 correct EducationController after comment techLead

--- src/main/java/school/faang/user_service/controller/education/EducationController.java
+++ src/main/java/school/faang/user_service/controller/education/EducationController.java
@@ -2,6 +2,7 @@ package school.faang.user_service.controller.education;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import school.faang.user_service.dto.EducationDto;
@@ -15,18 +16,21 @@ public class EducationController {
     private final EducationService educationService;
 
     @PostMapping("/{userId}")
-    public EducationDto addEducation(@PathVariable long userId, @Valid @RequestBody EducationDto educationDto) {
-        return educationService.addEducation(userId, educationDto);
+    public ResponseEntity<EducationDto> addEducation(@RequestParam("userId") long userId, @Valid @RequestBody EducationDto educationDto) {
+        EducationDto result = educationService.addEducation(userId, educationDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
     }
 
     @PutMapping("/{userId}")
-    public EducationDto updateEducation(@PathVariable long userId, @Valid @RequestBody EducationDto educationDto) {
-        return educationService.updateEducation(userId, educationDto);
+    public ResponseEntity<EducationDto> updateEducation(@RequestParam("userId") long userId, @Valid @RequestBody EducationDto educationDto) {
+        EducationDto result = educationService.updateEducation(userId, educationDto);
+        return ResponseEntity.ok(result);
     }
 
     @GetMapping("/{educationId}")
-    public EducationDto getById(@PathVariable long educationId) {
-        return educationService.getById(educationId);
+    public ResponseEntity<EducationDto> getById(@PathVariable("educationId") long educationId) {
+        EducationDto result = educationService.getById(educationId);
+        return ResponseEntity.ok(result);
     }
 
     @ExceptionHandler(DataValidationException.class)
