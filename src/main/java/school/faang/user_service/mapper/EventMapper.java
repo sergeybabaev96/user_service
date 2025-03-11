273fd142677ba687c31b8e1ad528bf58a38553a9
RamilNazmiev192 <nazmiev633@gmail.com>
2025-03-11 09:43:35 +0300

    refactor:EventMapper and  add EventMapperDecorator

--- src/main/java/school/faang/user_service/mapper/EventMapper.java
+++ src/main/java/school/faang/user_service/mapper/EventMapper.java
@@ -8,30 +8,23 @@ import school.faang.user_service.entity.event.Event;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = EventMapperDecorator.class)
 public interface EventMapper {
 
     @Mapping(source = "relatedSkills", target = "relatedSkills", qualifiedByName = "mapSkillsToIds")
     EventDto toDto(Event event);
 
-    @Mapping(source = "relatedSkills", target = "relatedSkills", qualifiedByName = "mapIdsToSkills")
+    @Mapping(source = "relatedSkills", target = "relatedSkills")
     Event toEntity(EventDto eventDto);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    @Mapping(source = "relatedSkills", target = "relatedSkills", qualifiedByName = "mapIdsToSkills")
+    @Mapping(source = "relatedSkills", target = "relatedSkills")
     void updateEventFormDto(EventDto eventDto, @MappingTarget Event event);
 
     @Named("mapSkillsToIds")
     static List<Long> mapSkillsToIds(List<Skill> skills) {
         return skills == null ? null : skills.stream()
                 .map(Skill::getId)
-                .collect(Collectors.toList());
-    }
-
-    @Named("mapIdsToSkills")
-    static List<Skill> mapIdsToSkills(List<Long> skillIds) {
-        return skillIds == null ? null : skillIds.stream()
-                .map(id -> Skill.builder().id(id).build())
                 .collect(Collectors.toList());
     }
 }
