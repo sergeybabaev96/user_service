96b827bfb4e106246278233f0667aa429ab828e4
Yaroslav Olkin <shrelok.pupkin@yandex.ru>
2025-03-12 21:40:56 +0300

    -

--- src/main/java/school/faang/user_service/utils/validationUtils/RecommendationValidation.java
+++ src/main/java/school/faang/user_service/utils/validationUtils/RecommendationValidation.java
@@ -11,8 +11,8 @@ import java.util.List;
 public class RecommendationValidation {
     private static final String CONTENT_NULL_EXCEPTION = "The content of the recommendation cannot be null";
     private static final String CONTENT_EMPTY_EXCEPTION = "The content of the recommendation cannot be empty";
-    private static final String DATE_EXCEPTION = "It is not possible to give a recommendation to this user because the"+
-            " last recommendation was offered less than 6 months ago.";
+    private static final String DATE_EXCEPTION = "It is not possible to give a recommendation to this user because"+
+            " the last recommendation was offered less than 6 months ago.";
     private static final String DATE_NULL_EXCEPTION = "The date of the recommendation cannot be null";
     private static final String RECOMMENDATION_NULL_EXCEPTION = "The recommendation cannot be null";
     private static final String SKILL_OFFER_VALID_EXCEPTION = "An offer to purchase a skill with the ID: " +
