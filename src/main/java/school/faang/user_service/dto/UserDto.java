189d3dcbbe63025243bc5fb560a6c68f2784478c
Pavel Chernobay <mr.chernobay1990@gmail.com>
2025-03-13 01:36:02 +0300

    Написал тесты для сервисов

--- src/main/java/school/faang/user_service/dto/UserDto.java
+++ src/main/java/school/faang/user_service/dto/UserDto.java
@@ -1,10 +1,12 @@
 package school.faang.user_service.dto;
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@Builder
 public class UserDto {
 
     private Long id;
