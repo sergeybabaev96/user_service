562354fd5b780e4e60d545cd4a2860c6c770a64e
Kirill Pankov <uralostrov6@gmail.com>
2025-03-12 22:05:41 +0500

    Добавил swagger, написал документацию для api

--- src/main/java/school/faang/user_service/controller/RecommendationController.java
+++ src/main/java/school/faang/user_service/controller/RecommendationController.java
@@ -1,5 +1,10 @@
 package school.faang.user_service.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
@@ -15,28 +20,70 @@ import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.recommendation.RecommendationDto;
 import school.faang.user_service.service.RecommendationService;
 
-import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
 public class RecommendationController {
     private final RecommendationService recommendationService;
 
+    @Operation(
+            summary = "Создание рекомендации",
+            description = "Создает новую рекомендацию с возможным списком предложенных навыков."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Рекомендация успешно создана",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = RecommendationDto.class))),
+            @ApiResponse(responseCode = "400", description = "Некорректные данные запроса",
+                    content = @Content(mediaType = "application/json"))
+    })
     @PostMapping("/recommendation/create")
     public RecommendationDto giveRecommendation(@RequestBody @Valid RecommendationDto recommendation) {
         return recommendationService.create(recommendation);
     }
 
+    @Operation(
+            summary = "Обновление рекомендации",
+            description = "Вносит изменения в уже существующую рекомендацию."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Рекомендация успешно обновлена",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = RecommendationDto.class))),
+            @ApiResponse(responseCode = "400", description = "Некорректные данные запроса",
+                    content = @Content(mediaType = "application/json"))
+    })
     @PutMapping("/recommendation/update")
     public RecommendationDto updateRecommendation(@RequestBody @Valid RecommendationDto updated) {
         return recommendationService.update(updated);
     }
 
+    @Operation(
+            summary = "Удаление рекомендации",
+            description = "Удаляет рекомендацию полностью"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Рекомендация успешно удалена",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = RecommendationDto.class))),
+            @ApiResponse(responseCode = "400", description = "Некорректные данные запроса",
+                    content = @Content(mediaType = "application/json"))
+    })
     @DeleteMapping("/recommendation/{recommendationId}/delete")
     public boolean deleteRecommendation(@PathVariable @Min(1) long recommendationId) {
         return recommendationService.delete(recommendationId);
     }
 
+    @Operation(
+            summary = "Рекомендаций оставленные пользователю",
+            description = "Вернет в виде страниц список рекомендаций пользователя."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Успешное выполнение!",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = RecommendationDto.class))),
+            @ApiResponse(responseCode = "400", description = "Некорректные данные запроса",
+                    content = @Content(mediaType = "application/json"))
+    })
     @GetMapping("/recommendation/{receiverId}/getAllUserRecommendations")
     public Page<RecommendationDto> getAllUserRecommendations(
             @PathVariable @Min(1) long receiverId,
@@ -46,6 +93,17 @@ public class RecommendationController {
         return recommendationService.getAllUserRecommendations(receiverId, page, size);
     }
 
+    @Operation(
+            summary = "Рекомендации от пользователя",
+            description = "Вернет список рекомендаций которые выдал пользователь"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Рекомендация успешно создана",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = RecommendationDto.class))),
+            @ApiResponse(responseCode = "400", description = "Некорректные данные запроса",
+                    content = @Content(mediaType = "application/json"))
+    })
     @GetMapping("/recommendation/{authorId}/getAllGivenRecommendations")
     public Page<RecommendationDto> getAllGivenRecommendations(
             @PathVariable @Min(1) long authorId,
