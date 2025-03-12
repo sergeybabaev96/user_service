4bd464b32293c17750564490a242a7cdb6dd1970
elsledkov <elsledkov@gmail.com>
2025-03-13 00:21:17 +0300

    Add tests  task BJS2-65898

--- src/main/java/school/faang/user_service/dto/UserDto.java
+++ src/main/java/school/faang/user_service/dto/UserDto.java
@@ -1,4 +1,16 @@
 package school.faang.user_service.dto;
 
-public record UserDto(long id, String username, String email) {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
+public final class UserDto {
+    private final long id;
+    private String username;
+    private  String email;
 }
---
+++ src/test/java/school/faang/user_service/controller/event/EventParticipationControllerTest.java
@@ -0,0 +1,47 @@
+package school.faang.user_service.controller.event;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import school.faang.user_service.service.event.EventParticipationService;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class EventParticipationControllerTest {
+    private final long EVENT_ID = 1L;
+    private final long USER_ID = 1L;
+
+    @Mock
+    private EventParticipationService participationService;
+
+    @InjectMocks
+    private EventParticipationController participationController;
+
+    @Test
+    void testRegisterParticipant() {
+        participationController.registerParticipant(EVENT_ID, USER_ID);
+        verify(participationService, times(1)).registerParticipant(EVENT_ID, USER_ID);
+    }
+
+    @Test
+    void testUnregisterParticipant() {
+        participationController.unregisterParticipant(EVENT_ID, USER_ID);
+        verify(participationService, times(1)).unregisterParticipant(EVENT_ID, USER_ID);
+    }
+
+    @Test
+    void testGetParticipant() {
+        participationController.getParticipant(EVENT_ID);
+        verify(participationService, times(1)).getParticipant(EVENT_ID);
+    }
+
+    @Test
+    void getParticipantsCount() {
+        participationController.getParticipantsCount(EVENT_ID);
+        verify(participationService, times(1)).getParticipantsCount(EVENT_ID);
+    }
+}
