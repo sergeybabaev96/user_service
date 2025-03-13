54b1a2c9229e81c4fe49255233c2029babaf8eba
Marsel <mars1212@inbox.ru>
2025-03-14 00:44:34 +0300

    BJS2-65863-write-recommendation убрал 2 метода с маппера

--- src/main/java/school/faang/user_service/mapper/RecommendationMapper.java
+++ src/main/java/school/faang/user_service/mapper/RecommendationMapper.java
@@ -8,25 +8,14 @@ import school.faang.user_service.entity.recommendation.Recommendation;
 
 @Mapper(componentModel = "spring", uses = {SkillOfferMapper.class})
 public interface RecommendationMapper {
-    //This mapper for work with ViewDto
-    @Mapping(target = "author", ignore = true)
-    @Mapping(target = "receiver", ignore = true)
-    @Mapping(source = "skillOffers", target = "skillOffers")
-    Recommendation viewDtoToEntity(RecommendationViewDto recommendation);
 
     @Mapping(source = "author.id", target = "authorId")
     @Mapping(source = "receiver.id", target = "receiverId")
     @Mapping(source = "skillOffers", target = "skillOffers")
     RecommendationViewDto toViewDto(Recommendation recommendation);
 
-    //This mapper for work with CreateDto
     @Mapping(target = "author", ignore = true)
     @Mapping(target = "receiver", ignore = true)
     @Mapping(source = "skillOffers", target = "skillOffers")
     Recommendation createDtoToEntity(RecommendationCreateDto recommendation);
-
-    @Mapping(source = "author.id", target = "authorId")
-    @Mapping(source = "receiver.id", target = "receiverId")
-    @Mapping(source = "skillOffers", target = "skillOffers")
-    RecommendationCreateDto toCreateDto(Recommendation recommendation);
 }
