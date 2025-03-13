5e77e6fc0aafbb213e4d11ada28943a8c9d60938
Valerii Enin <enin_v_a@mail.ru>
2025-03-13 23:08:21 +0300

    BJS2-66801. Тесты на задачу "Стать ментором"

--- src/main/java/school/faang/user_service/mapper/EducationMapper.java
+++ src/main/java/school/faang/user_service/mapper/EducationMapper.java
@@ -3,11 +3,12 @@ package school.faang.user_service.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.ReportingPolicy;
 import school.faang.user_service.dto.EducationDto;
 import school.faang.user_service.entity.Education;
 import school.faang.user_service.entity.User;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface EducationMapper {
 
     Education toEducation(EducationDto educationDto);
@@ -17,5 +18,9 @@ public interface EducationMapper {
     @Mapping(target = "user", ignore = true)
     void updateEducationFromDto(EducationDto dto, @MappingTarget Education entity);
 
-    Education toEducationWithUser(EducationDto educationDto, User user);
+    default Education toEducationWithUser(EducationDto educationDto, User user) {
+        Education education = toEducation(educationDto);
+        education.setUser(user);
+        return education;
+    }
 }
---
+++ src/test/java/school/faang/user_service/controller/mentorship/MentorshipControllerTest.java
@@ -0,0 +1,109 @@
+package school.faang.user_service.controller.mentorship;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import school.faang.user_service.config.context.UserContext;
+import school.faang.user_service.dto.MentorshipDto;
+import school.faang.user_service.service.MentorshipService;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(MentorshipController.class)
+@Import(MentorshipControllerTest.TestExceptionHandler.class)
+public class MentorshipControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private MentorshipService mentorshipService;
+
+    @MockBean
+    private UserContext userContext;
+
+    @Test
+    public void testGetMenteesSuccess() throws Exception {
+        long mentorId = 1L;
+        MentorshipDto mentorshipDto1 = new MentorshipDto();
+        mentorshipDto1.setUserId(2L);
+        mentorshipDto1.setUserName("mentee1");
+        MentorshipDto mentorshipDto2 = new MentorshipDto();
+        mentorshipDto2.setUserId(3L);
+        mentorshipDto2.setUserName("mentee2");
+        List<MentorshipDto> mentees = Arrays.asList(mentorshipDto1, mentorshipDto2);
+
+        when(mentorshipService.getMentees(mentorId)).thenReturn(mentees);
+
+        mockMvc.perform(get("/mentorship/mentors/{mentorId}/mentees", mentorId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].userId").value(2L))
+                .andExpect(jsonPath("$[0].userName").value("mentee1"))
+                .andExpect(jsonPath("$[1].userId").value(3L))
+                .andExpect(jsonPath("$[1].userName").value("mentee2"));
+    }
+
+    @Test
+    public void testGetMenteesEmptyList() throws Exception {
+        long mentorId = 1L;
+        when(mentorshipService.getMentees(mentorId)).thenReturn(Collections.emptyList());
+
+        mockMvc.perform(get("/mentorship/mentors/{mentorId}/mentees", mentorId))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]"));
+    }
+
+    @Test
+    void deleteMenteeSuccessfulDeletionReturnsNoContent() throws Exception {
+        long mentorId = 1L;
+        long menteeId = 2L;
+        doNothing().when(mentorshipService).deleteMentee(menteeId, mentorId);
+
+        mockMvc.perform(delete("/mentorship/mentors/{mentorId}/mentees/{menteeId}", mentorId, menteeId))
+                .andExpect(status().isNoContent());
+
+        verify(mentorshipService).deleteMentee(menteeId, mentorId);
+    }
+
+    @Test
+    public void deleteMenteeWhenServiceThrowsExceptionReturnsNotFound() throws Exception {
+        long mentorId = 1L;
+        long menteeId = 2L;
+        doThrow(new EntityNotFoundException("Some error")).when(mentorshipService).deleteMentee(menteeId, mentorId);
+
+        mockMvc.perform(delete("/mentorship/mentors/{mentorId}/mentees/{menteeId}", mentorId, menteeId))
+                .andExpect(status().isNotFound());
+
+        verify(mentorshipService).deleteMentee(menteeId, mentorId);
+    }
+
+    @ControllerAdvice
+    static class TestExceptionHandler {
+        @ExceptionHandler(EntityNotFoundException.class)
+        @ResponseStatus(HttpStatus.NOT_FOUND)
+        public void handleEntityNotFoundException(EntityNotFoundException ex) {
+        }
+    }
+}
