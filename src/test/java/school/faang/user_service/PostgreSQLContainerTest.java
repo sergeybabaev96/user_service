86435c71d895a7a7608aa8f79a97ec2b9b769e61
Mipusatina <mipspam@mail.ru>
2025-03-13 17:42:50 +0100

    PostgreSQLContainerTest. @DynamicPropertySource

--- src/test/java/school/faang/user_service/PostgreSQLContainerTest.java
+++ src/test/java/school/faang/user_service/PostgreSQLContainerTest.java
@@ -1,25 +1,52 @@
 package school.faang.user_service;
 
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import static org.junit.Assert.assertTrue;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @Testcontainers
+@RequiredArgsConstructor
 public class PostgreSQLContainerTest {
+
     @Container
-    public PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>()
+    public static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15")
             .withDatabaseName("test")
             .withUsername("user")
             .withPassword("password");
 
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.datasource.driver-class-name", () -> "org.postgresql.Driver");
+    }
+
+    private final DataSource dataSource;
+
     @Test
-    void testDatabaseConnection() {
+    void testDatabaseIsRunning() {
         System.out.println("PostgreSQL JDBC URL: " + postgres.getJdbcUrl());
         System.out.println("PostgreSQL Username: " + postgres.getUsername());
         System.out.println("PostgreSQL Password: " + postgres.getPassword());
         assertTrue(postgres.isRunning());
+    }
+
+    @Test
+    void testDatabaseConnection() throws SQLException {
+        try (Connection conn = dataSource.getConnection()) {
+            assertFalse(conn.isClosed());
+        }
     }
 }
