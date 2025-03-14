70dccdb5d5fbf6b44e9c2de4515e29f07d408934
pstratonni <79229338+pstratonni@users.noreply.github.com>
2025-03-14 06:55:57 +0100

    Task BJS2-65897-event-organization added create

---
+++ src/main/java/school/faang/user_service/controller/EventController.java
@@ -0,0 +1,30 @@
+package school.faang.user_service.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import school.faang.user_service.dto.event.EventDto;
+import school.faang.user_service.exception.DataValidationException;
+import school.faang.user_service.service.event.EventService;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Controller
+@RequiredArgsConstructor
+public class EventController {
+    private final EventService eventService;
+
+    public EventDto create(EventDto event) {
+        validateEvent(event);
+        return eventService.create(event);
+    }
+
+    private void validateEvent(EventDto event) {
+        if (event == null || event.getTitle().isBlank() || !Objects.nonNull(event.getStartDate())
+                || event.getStartDate().isBefore(LocalDateTime.now())
+                || event.getOwnerId() == null ) {
+            throw new DataValidationException("Event not confirmed");
+        }
+    }
+}
