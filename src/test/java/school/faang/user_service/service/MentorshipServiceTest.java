b806c081213e73602aef74e2e63c25a069544485
elsledkov <elsledkov@gmail.com>
2025-03-15 22:54:42 +0300

    Resolve merge conflicts task: BJS2-65898

--- src/test/java/school/faang/user_service/service/MentorshipServiceTest.java
+++ src/test/java/school/faang/user_service/service/MentorshipServiceTest.java
@@ -13,7 +13,9 @@ import school.faang.user_service.repository.mentorship.MentorshipRepository;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
