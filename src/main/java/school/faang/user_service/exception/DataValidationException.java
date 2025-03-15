776b674f6f7d7991d9c57cbe2a2993bc55ae0d57
Alexti25 <alxej.kazakov@gmail.com>
2025-03-15 15:06:14 +0300

    IllegalArgumentException

--- src/main/java/school/faang/user_service/exception/DataValidationException.java
+++ src/main/java/school/faang/user_service/exception/DataValidationException.java
@@ -1,6 +1,6 @@
 package school.faang.user_service.exception;
 
-public class DataValidationException extends RuntimeException {
+public class DataValidationException extends IllegalArgumentException {
     public DataValidationException(String message) {
         super(message);
     }
