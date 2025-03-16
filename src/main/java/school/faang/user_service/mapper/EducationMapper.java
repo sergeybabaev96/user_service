65a735155a63dc9384e5b9e6986621e401dea789
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-03-16 11:47:50 +0500

    https://faang-school.atlassian.net/browse/BJS2-66800
    Test CareerService

--- src/main/java/school/faang/user_service/mapper/EducationMapper.java
+++ src/main/java/school/faang/user_service/mapper/EducationMapper.java
@@ -1,13 +1,11 @@
 package school.faang.user_service.mapper;
 
-import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import school.faang.user_service.dto.EducationDto;
 import school.faang.user_service.entity.Education;
 import school.faang.user_service.entity.User;
 
-@Mapper(componentModel = "spring")
 public interface EducationMapper {
 
     Education toEducation(EducationDto educationDto);
@@ -17,5 +15,6 @@ public interface EducationMapper {
     @Mapping(target = "user", ignore = true)
     void updateEducationFromDto(EducationDto dto, @MappingTarget Education entity);
 
+    @Mapping(source = "educationDto.id", target = "id") // Добавляем аннотацию
     Education toEducationWithUser(EducationDto educationDto, User user);
 }
