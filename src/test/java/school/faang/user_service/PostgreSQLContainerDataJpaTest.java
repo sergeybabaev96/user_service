ca2441d08d42bf2cf2173dc472b7a934a7a6f4bf
Mipusatina <mipspam@mail.ru>
2025-03-17 23:54:51 +0100

    Delete from PostgreSQLContainerDataJpaTest.java @annotations

--- src/test/java/school/faang/user_service/PostgreSQLContainerDataJpaTest.java
+++ src/test/java/school/faang/user_service/PostgreSQLContainerDataJpaTest.java
@@ -9,16 +9,16 @@ import javax.sql.DataSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest
-@ContextConfiguration(classes = PostgreSQLContainerConfiguration.class)
+//@DataJpaTest
+//@ContextConfiguration(classes = PostgreSQLContainerConfiguration.class)
 public class PostgreSQLContainerDataJpaTest extends PostgreSQLContainerConfiguration {
 
-    @Autowired
-    private DataSource dataSource;
-
-    @Test
-    void testDatabaseConnection() throws Exception {
-        assertThat(dataSource).isNotNull();
-        System.out.println("✅ DB is reachable!");
-    }
+//    @Autowired
+//    private DataSource dataSource;
+//
+//    @Test
+//    void testDatabaseConnection() throws Exception {
+//        assertThat(dataSource).isNotNull();
+//        System.out.println("✅ DB is reachable!");
+//    }
 }
