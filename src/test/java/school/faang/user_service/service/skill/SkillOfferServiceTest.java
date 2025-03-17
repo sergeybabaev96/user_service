360a700c1127921824ca4f62c02c9061235614da
pstratonni <79229338+pstratonni@users.noreply.github.com>
2025-03-17 17:32:31 +0100

    Task BJS2-66762-test-creating-and-customizing-skills add SkillOfferServiceTest and SkillUserGuaranteeTest

--- src/test/java/school/faang/user_service/service/skill/SkillOfferServiceTest.java
+++ src/test/java/school/faang/user_service/service/skill/SkillOfferServiceTest.java
@@ -1,9 +1,59 @@
 package school.faang.user_service.service.skill;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import school.faang.user_service.entity.recommendation.SkillOffer;
+import school.faang.user_service.exception.DataValidationException;
+import school.faang.user_service.repository.recommendation.SkillOfferRepository;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class SkillOfferServiceTest {
+
+    @Mock
+    private SkillOfferRepository skillOfferRepository;
+
+    @InjectMocks
+    private SkillOfferService skillOfferService;
+
+    @Test
+    public void testIsEnoughAmountOffersToSkillWithWrong() {
+        long skillOfferId = 1L;
+        long userId = 1L;
+        SkillOffer skillOffered1 = new SkillOffer();
+        SkillOffer skillOffered2 = new SkillOffer();
+        skillOffered1.setId(skillOfferId);
+        skillOffered2.setId(skillOfferId);
+        List<SkillOffer> skillOffers = List.of(skillOffered1, skillOffered2);
+        when(skillOfferRepository.findAllOffersOfSkill(skillOfferId, userId)).thenReturn(skillOffers);
+        assertThrows(DataValidationException.class, () -> skillOfferService.isEnoughAmountOffersToSkill(skillOfferId, userId));
+    }
+
+    @Test
+    public void testIsEnoughAmountOffersToSkill() {
+        long skillOfferId = 1L;
+        long userId = 1L;
+        SkillOffer skillOffered1 = new SkillOffer();
+        SkillOffer skillOffered2 = new SkillOffer();
+        SkillOffer skillOffered3 = new SkillOffer();
+        skillOffered1.setId(skillOfferId);
+        skillOffered2.setId(skillOfferId);
+        skillOffered3.setId(skillOfferId);
+        List<SkillOffer> skillOffers = List.of(skillOffered1, skillOffered2, skillOffered3);
+        when(skillOfferRepository.findAllOffersOfSkill(skillOfferId, userId)).thenReturn(skillOffers);
+        skillOfferService.isEnoughAmountOffersToSkill(skillOfferId, userId);
+        verify(skillOfferRepository, times(1)).findAllOffersOfSkill(skillOfferId, userId);
+
+
+    }
 
 }
---
+++ src/test/java/school/faang/user_service/service/skill/SkillUserGuaranteeTest.java
@@ -0,0 +1,55 @@
+package school.faang.user_service.service.skill;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import school.faang.user_service.entity.Skill;
+import school.faang.user_service.entity.User;
+import school.faang.user_service.entity.recommendation.Recommendation;
+import school.faang.user_service.entity.recommendation.SkillOffer;
+import school.faang.user_service.repository.UserSkillGuaranteeRepository;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+public class SkillUserGuaranteeTest {
+
+    @Mock
+    private UserSkillGuaranteeRepository guaranteeRepository;
+    @Mock
+    private SkillOfferService skillOfferService;
+
+    @InjectMocks
+    private SkillUserGuarantee skillUserGuarantee;
+
+    @Test
+    public void testAddUserSkillGuarantee() {
+        long skillId = 1;
+        long userId = 1;
+        SkillOffer offer1 = new SkillOffer();
+        SkillOffer offer2 = new SkillOffer();
+        Skill skill = new Skill();
+        offer1.setId(skillId);
+        offer2.setId(userId);
+        offer1.setSkill(skill);
+        offer2.setSkill(skill);
+        Recommendation recommendation = new Recommendation();
+        User user = new User();
+        recommendation.setAuthor(user);
+        offer1.setRecommendation(recommendation);
+        offer2.setRecommendation(recommendation);
+        List<SkillOffer> skillOffers = List.of(offer1, offer2);
+        when(skillOfferService.getSkillOfferToUser(skillId, userId)).thenReturn(skillOffers);
+        skillUserGuarantee.addUserSkillGuarantee(skillId, userId);
+        verify(skillOfferService,times(1)).getSkillOfferToUser(skillId, userId);
+        verify(guaranteeRepository, times(2)).save(any());
+    }
+}
