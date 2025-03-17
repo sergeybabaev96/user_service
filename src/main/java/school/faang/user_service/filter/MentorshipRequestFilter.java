47e2e7aaea253acfeae0dcfdb5c18761f9ae2878
Valeron <enin_v_a@mail.ru>
2025-03-17 22:15:29 +0300

    Исправил баги задачи Запрос на менторство

--- src/main/java/school/faang/user_service/filter/MentorshipRequestFilter.java
+++ src/main/java/school/faang/user_service/filter/MentorshipRequestFilter.java
@@ -7,6 +7,7 @@ import java.util.stream.Stream;
 
 public interface MentorshipRequestFilter {
     boolean isApplyable(RequestFilterDto requestFilterDto);
+
     Stream<MentorshipRequest> filter(Stream<MentorshipRequest> requests,
                                      RequestFilterDto filter);
 }
