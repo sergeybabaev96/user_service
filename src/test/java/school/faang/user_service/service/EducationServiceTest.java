492c25e3751b9c371e37bb229316eb337fcc2e1c
Anton Lukyanchikov <antontotenkopf@gmail.com>
2025-03-18 19:11:33 +0300

    тест testAddEducation() иtestUpdateEducation() успешно прошел

--- src/test/java/school/faang/user_service/service/EducationServiceTest.java
+++ src/test/java/school/faang/user_service/service/EducationServiceTest.java
@@ -4,12 +4,15 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mapstruct.factory.Mappers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 import school.faang.user_service.dto.EducationViewDto;
+import school.faang.user_service.entity.Education;
 import school.faang.user_service.entity.User;
 import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.mapper.EducationMapper;
@@ -18,29 +21,42 @@ import school.faang.user_service.repository.UserRepository;
 import school.faang.user_service.service.education.EducationService;
 
 import java.time.Year;
+import java.util.List;
 import java.util.Optional;
 
+@ExtendWith(MockitoExtension.class)
 public class EducationServiceTest {
+    @InjectMocks
+    private EducationService educationService;
+
     @Mock
     private UserRepository userRepository;
+
     @Mock
     private EducationRepository educationRepository;
+
     @Spy
     private EducationMapper educationMapper = Mappers.getMapper(EducationMapper.class);
 
-    @InjectMocks
-    private EducationService educationService;
-    private User user;
+    private User user = new User();
     private User anotherUser;
+    private Education education = new Education();
+    private EducationViewDto educationViewDto = new EducationViewDto();
 
     @BeforeEach
     void setUp() {
-        user = new User();
         user.setId(1L);
+        user.setEducation(List.of(education));
+        education.setUser(user);
+        educationViewDto.setId(10L);
+        educationViewDto.setYearFrom(Year.now().getValue());
+        educationViewDto.setYearTo(Year.now().getValue() + 4);
+        educationViewDto.setInstitution("");
+        educationViewDto.setEducationLevel("");
+        educationViewDto.setSpecialization("");
 
         anotherUser = new User();
         anotherUser.setId(2L);
-        educationService = new EducationService(userRepository, educationRepository, educationMapper);
     }
 
     @DisplayName("Проверка получения ошибки при указании года начала обучения больше текущего")
@@ -53,39 +69,31 @@ public class EducationServiceTest {
         Assertions.assertThrows(DataValidationException.class, () -> educationService.addEducation(userId, educationDto));
     }
 
-    @DisplayName("Проверка успешного прохождения валидации года начала обучения")
-    @Test
-    void testAddEducationWithCorrectYearFrom() {
-
-    }
-
     @DisplayName("Проверка получения ошибки при попытке найти пользователя по несуществующему id")
     @Test
     void testAddEducationWithNonExistingUserId() {
-
-    }
-
-    @DisplayName("Проверка успешного нахождения пользователя по существующему id")
-    @Test
-    void testAddEducationWithExistingUserId() {
-
+        EducationViewDto educationDto = new EducationViewDto();
+        educationDto.setYearFrom(Year.now().getValue());
+        long userId = 5L;
+        Assertions.assertThrows(DataValidationException.class, () -> educationService.addEducation(userId, educationDto));
     }
 
     @DisplayName("Проверка успешного добавления данных об образовании")
     @Test
     void testAddEducation() {
-
+        Mockito.when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
+        Mockito.when(educationMapper.toEducation(educationViewDto)).thenReturn(education);
+        Mockito.when(educationRepository.save(education)).thenReturn(education);
+        Mockito.when(educationMapper.toEducationDto(education)).thenReturn(educationViewDto);
+        EducationViewDto result = educationService.addEducation(user.getId(), educationViewDto);
+        Assertions.assertEquals(educationViewDto, result);
     }
+
+
 
     @DisplayName("Проверка получения ошибки при попытке найти данные об образовании по несуществующему id")
     @Test
     void testUpdateEducationWithNonExistingEducationId() {
-
-    }
-
-    @DisplayName("Проверка успешного нахождения данных об образовании по существующему id")
-    @Test
-    void testUpdateEducationWithExistingEducationId() {
 
     }
 
@@ -95,16 +103,19 @@ public class EducationServiceTest {
 
     }
 
-    @DisplayName("Успешное прохождение проверки на попытку обновления данных об образовании третьим лицом")
-    @Test
-    void testUpdateEducationWithSuccessfulValidateUser() {
-
-    }
 
     @DisplayName("Проверка успешного обновления данных об образовании")
     @Test
     void testUpdateEducation() {
+        Mockito.when(educationRepository.findById(10L)).thenReturn(Optional.of(education));
+        Mockito.when(educationMapper.toEducationDto(education)).thenReturn(educationViewDto);
+        Mockito.when(educationMapper.toEducation(educationViewDto)).thenReturn(education);
+        Mockito.when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
+        Mockito.when(educationRepository.save(education)).thenReturn(education);
+        Mockito.when(educationMapper.toEducationDto(education)).thenReturn(educationViewDto);
+        EducationViewDto result = educationService.updateEducation(user.getId(), educationViewDto);
 
+        Assertions.assertEquals(educationViewDto, result);
     }
 
 }
