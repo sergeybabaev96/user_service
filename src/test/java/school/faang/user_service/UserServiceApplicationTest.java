f9f6c5742ba53f88a645d4cb3e5ad8effd47aac0
Mipusatina <mipspam@mail.ru>
2025-03-18 01:16:58 +0100

    2nd @SpringBootTest

--- src/test/java/school/faang/user_service/UserServiceApplicationTest.java
+++ src/test/java/school/faang/user_service/UserServiceApplicationTest.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-//@SpringBootTest
+@SpringBootTest
 class UserServiceApplicationTest {
 
     @Test
