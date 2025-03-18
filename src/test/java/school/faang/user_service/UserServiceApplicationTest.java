b4cde613bcd150299afa1b64c7b16902a32f1233
Mipusatina <mipspam@mail.ru>
2025-03-18 01:35:29 +0100

    UserServiceApplicatoinTest @EnableJpaRepositories

--- src/test/java/school/faang/user_service/UserServiceApplicationTest.java
+++ src/test/java/school/faang/user_service/UserServiceApplicationTest.java
@@ -2,12 +2,14 @@ package school.faang.user_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import school.faang.user_service.repository.UserRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,6 +21,8 @@ import static org.junit.jupiter.api.Assertions.*;
         HibernateJpaAutoConfiguration.class,
         LiquibaseAutoConfiguration.class
 })
+@EnableJpaRepositories("school.faang.user_service.repository") // ✅ Сканирует все репозитории внутри этого пакета
+@EntityScan("school.faang.user_service.model")
 class UserServiceApplicationTest {
 
     @MockBean
