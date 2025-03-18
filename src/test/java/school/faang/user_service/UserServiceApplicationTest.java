332b12dc68d997bfadc6400e71b3b9bbf6e7af69
Mipusatina <mipspam@mail.ru>
2025-03-18 01:48:32 +0100

    UserServiceApplicatoinTest extens PostgreSQLContainerSpringBootTest

--- src/test/java/school/faang/user_service/UserServiceApplicationTest.java
+++ src/test/java/school/faang/user_service/UserServiceApplicationTest.java
@@ -1,32 +1,11 @@
 package school.faang.user_service;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import school.faang.user_service.repository.UserRepository;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@ImportAutoConfiguration(exclude = {
-        DataSourceAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class,
-        LiquibaseAutoConfiguration.class
-})
-@EnableJpaRepositories("school.faang.user_service.repository") // ✅ Сканирует все репозитории внутри этого пакета
-@EntityScan("school.faang.user_service.entity")
-class UserServiceApplicationTest {
-
-    @MockBean
-    private UserRepository userRepository;
+public class UserServiceApplicationTest extends PostgreSQLContainerSpringBootTest{
 
     @Test
     void contextLoads() {
