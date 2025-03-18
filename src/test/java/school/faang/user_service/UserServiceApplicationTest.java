9a391653a9a7fba04af55bdac1561bddb4a24867
Mipusatina <mipspam@mail.ru>
2025-03-18 01:25:19 +0100

    UserServiceApplicatoinTest without Testcontainers

--- src/test/java/school/faang/user_service/UserServiceApplicationTest.java
+++ src/test/java/school/faang/user_service/UserServiceApplicationTest.java
@@ -1,11 +1,22 @@
 package school.faang.user_service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ImportAutoConfiguration(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class,
+        LiquibaseAutoConfiguration.class
+})
 class UserServiceApplicationTest {
 
     @Test
