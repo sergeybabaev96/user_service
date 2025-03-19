6565e9e0f607507be7fce06026cc40280d73a8dc
Tony King <a.korolev9927@gmail.com>
2025-03-20 00:09:28 +0300

    Внес правки в SkillMapper

--- src/main/java/school/faang/user_service/mapper/SkillMapper.java
+++ src/main/java/school/faang/user_service/mapper/SkillMapper.java
@@ -1,15 +1,24 @@
 package school.faang.user_service.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import school.faang.user_service.dto.skill.SkillCandidateDto;
 import school.faang.user_service.dto.skill.SkillDto;
 import school.faang.user_service.entity.Skill;
 
 @Mapper(componentModel = "spring" )
 public interface SkillMapper {
+
     SkillDto toDto(Skill skill);
 
+    @Mapping(target = "users", ignore = true)
+    @Mapping(target = "guarantees", ignore = true)
+    @Mapping(target = "events", ignore = true)
+    @Mapping(target = "goals", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
     Skill toEntity(SkillDto skillDto);
 
+    @Mapping(target = "offerAmount", ignore = true)
     SkillCandidateDto toSkillCandidateDto(Skill skill);
 }
