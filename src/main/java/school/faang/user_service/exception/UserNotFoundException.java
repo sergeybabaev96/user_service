17f9916b9da5e17d927b0e358244b404f1694f10
Ilyas Azhdar <azhdar902@gmail.com>
2025-03-20 00:10:31 +0500

    added GlobalExceptionHandler and UserNotFoundException

---
+++ src/main/java/school/faang/user_service/exception/UserNotFoundException.java
@@ -0,0 +1,14 @@
+package school.faang.user_service.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+
+public class UserNotFoundException extends EntityNotFoundException  {
+
+    public UserNotFoundException(Long id) {
+        super(String.format("User with id = %d doesn't exist", id));
+    }
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
