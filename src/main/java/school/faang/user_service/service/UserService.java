82995a13afb50a04abf151bf21581c8dc2c0ddc1
Ilyas Azhdar <azhdar902@gmail.com>
2025-03-20 00:10:55 +0500

    added getuserbyid and getusersbyids

--- src/main/java/school/faang/user_service/service/UserService.java
+++ src/main/java/school/faang/user_service/service/UserService.java
@@ -2,16 +2,43 @@ package school.faang.user_service.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.entity.User;
+import school.faang.user_service.exception.UserNotFoundException;
+import school.faang.user_service.mapper.UserMapper;
 import school.faang.user_service.repository.UserRepository;
+
+import java.util.Collections;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     public User getUserById(long id) {
         return userRepository.findById(id).orElseThrow(()
                 -> new RuntimeException("User with id " + id + " not found"));
+    }
+
+    public UserDto findUserById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(id));
+
+        return userMapper.toDto(user);
+    }
+
+    public List<UserDto> getUsersByIds(List<Long> ids) {
+        List<User> users = userRepository.findAllById(ids);
+
+        if (users.isEmpty()) {
+            System.out.println("Users notfoun");
+            return Collections.emptyList();
+        } else {
+            return users.stream()
+                    .map(userMapper::toDto)
+                    .toList();
+        }
     }
 }
