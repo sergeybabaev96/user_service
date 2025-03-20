6e9544287a0e2a66f3a5c70961cb7c505981d8d0
Sergey <bigwave2915@gmail.com>
2025-03-20 21:35:57 +0500

    сделал Тесты EventParticipationController

--- src/main/java/school/faang/user_service/controller/event/EventParticipationController.java
+++ src/main/java/school/faang/user_service/controller/event/EventParticipationController.java
@@ -1,37 +1,53 @@
 package school.faang.user_service.controller.event;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 import school.faang.user_service.dto.user.UserDto;
 import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.service.event.EventParticipationService;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/events")
 public class EventParticipationController {
     private final EventParticipationService eventParticipationService;
 
-    public void registerParticipant(long eventId, long userId) {
+    @PutMapping("/{eventId}/users/{userId}/register")
+    public void registerParticipant(@PathVariable long eventId,
+                                    @PathVariable long userId) {
         validateId(eventId);
         validateId(userId);
+        log.info("New request to register user with id: {} for event with id: {}", userId, eventId);
         eventParticipationService.registerParticipant(eventId, userId);
+        log.info("User with id: {} was registered for event with id: {}", userId, eventId);
+
     }
 
-    public void unregisterParticipant(long eventId, long userId) {
+    @DeleteMapping("/{eventId}/users/{userId}/unregister")
+    public void unregisterParticipant(@PathVariable long eventId,
+                                      @PathVariable long userId) {
         validateId(eventId);
         validateId(userId);
+        log.info("New request to unregister user with id: {} from event with id: {}", userId, eventId);
         eventParticipationService.unregisterParticipant(eventId, userId);
+        log.info("User with id: {} was unregistered from event with id: {}", userId, eventId);
     }
 
-    public List<UserDto> getParticipants(long eventId) {
+    @GetMapping("/{eventId}/participants")
+    public List<UserDto> getParticipants(@PathVariable long eventId) {
         validateId(eventId);
+        log.info("Get participants of event with id: {}", eventId);
         return eventParticipationService.getParticipants(eventId);
     }
 
-    public int getParticipantsCount(long eventId) {
+    @GetMapping("/{eventId}/count_participants")
+    public int getParticipantsCount(@PathVariable long eventId) {
         validateId(eventId);
+        log.info("Get number participants of event with id: {}", eventId);
         return eventParticipationService.getParticipantsCount(eventId);
     }
 
@@ -41,3 +57,4 @@ public class EventParticipationController {
         }
     }
 }
+
--- src/test/java/school/faang/user_service/controller/event/EventParticipationControllerTest.java
+++ src/test/java/school/faang/user_service/controller/event/EventParticipationControllerTest.java
@@ -1,19 +1,29 @@
 package school.faang.user_service.controller.event;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import school.faang.user_service.dto.user.UserDto;
 import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.service.event.EventParticipationService;
 
-import java.util.stream.Stream;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import java.util.Arrays;
+import java.util.List;
+
 
 @ExtendWith(MockitoExtension.class)
 public class EventParticipationControllerTest {
@@ -23,59 +33,138 @@ public class EventParticipationControllerTest {
     @InjectMocks
     private EventParticipationController eventParticipationController;
 
-    @ParameterizedTest
-    @MethodSource("provideInvalidIdsForExceptionTests")
-    public void testInvalidOperations(long eventId, long userId, String operation) {
-        switch (operation) {
-            case "register" -> assertThrows(DataValidationException.class,
-                    () -> eventParticipationController.registerParticipant(eventId, userId));
-            case "unregister" -> assertThrows(DataValidationException.class,
-                    () -> eventParticipationController.unregisterParticipant(eventId, userId));
-            case "getParticipants" -> assertThrows(DataValidationException.class,
-                    () -> eventParticipationController.getParticipants(eventId));
-            case "getParticipantsCount" -> assertThrows(DataValidationException.class,
-                    () -> eventParticipationController.getParticipantsCount(eventId));
-        }
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(eventParticipationController).build();
     }
 
-    @ParameterizedTest
-    @MethodSource("provideValidIdsForSuccessTests")
-    public void testValidOperations(long eventId, long userId, String operation) {
-        switch (operation) {
-            case "register" -> {
-                eventParticipationController.registerParticipant(eventId, userId);
-                verify(eventParticipationService, times(1)).registerParticipant(eventId, userId);
-            }
-            case "unregister" -> {
-                eventParticipationController.unregisterParticipant(eventId, userId);
-                verify(eventParticipationService, times(1)).unregisterParticipant(eventId, userId);
-            }
-            case "getParticipants" -> {
-                eventParticipationController.getParticipants(eventId);
-                verify(eventParticipationService, times(1)).getParticipants(eventId);
-            }
-            case "getParticipantsCount" -> {
-                eventParticipationController.getParticipantsCount(eventId);
-                verify(eventParticipationService, times(1)).getParticipantsCount(eventId);
-            }
-        }
+    @Test
+    void registerParticipation() throws Exception {
+        doNothing().when(eventParticipationService).registerParticipant(123L, 456L);
+
+        this.mockMvc.perform(put("/events/123/users/456/register"))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        verify(eventParticipationService).registerParticipant(123L, 456L);
     }
 
-    private static Stream<Arguments> provideInvalidIdsForExceptionTests() {
-        return Stream.of(
-                Arguments.of(-1, -1, "register"),
-                Arguments.of(-1, -1, "unregister"),
-                Arguments.of(-1, -1, "getParticipants"),
-                Arguments.of(-1, -1, "getParticipantsCount")
-        );
+    @Test
+    void registerParticipant_ValidData_Success() {
+        long eventId = 123L;
+        long userId = 456L;
+        eventParticipationController.registerParticipant(eventId, userId);
+        verify(eventParticipationService, times(1)).registerParticipant(eventId, userId);
     }
 
-    private static Stream<Arguments> provideValidIdsForSuccessTests() {
-        return Stream.of(
-                Arguments.of(1, 1, "register"),
-                Arguments.of(1, 1, "unregister"),
-                Arguments.of(1, 1, "getParticipants"),
-                Arguments.of(1, 1, "getParticipantsCount")
-        );
+    @Test
+    void registerParticipant_Invalid_ThrowsException() {
+        // Arrange
+        long eventId = -1L;
+        long userId = 2L;
+
+        DataValidationException exception = assertThrows(DataValidationException.class, () -> {
+            eventParticipationController.registerParticipant(eventId, userId);
+        });
+
+        assertEquals("Id is less than zero", exception.getMessage());
+        verify(eventParticipationService, never()).registerParticipant(anyLong(), anyLong());
+    }
+
+    @Test
+    void unregisterParticipation() throws Exception {
+        doNothing().when(eventParticipationService).unregisterParticipant(123L, 456L);
+
+        this.mockMvc.perform(delete("/events/123/users/456/unregister"))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        verify(eventParticipationService).unregisterParticipant(123L, 456L);
+    }
+
+    @Test
+    void unregisterParticipant_ValidData_Success() {
+        long eventId = 123L;
+        long userId = 456L;
+        eventParticipationController.unregisterParticipant(eventId, userId);
+        verify(eventParticipationService, times(1)).unregisterParticipant(eventId, userId);
+    }
+
+    @Test
+    void unregisterParticipant_Invalid_ThrowsException() {
+        long eventId = -1L;
+        long userId = 2L;
+
+        DataValidationException exception = assertThrows(DataValidationException.class, () -> {
+            eventParticipationController.unregisterParticipant(eventId, userId);
+        });
+
+        assertEquals("Id is less than zero", exception.getMessage());
+        verify(eventParticipationService, never()).unregisterParticipant(anyLong(), anyLong());
+    }
+
+    @Test
+    void getParticipants_Success() {
+        long eventId = 1L;
+
+        UserDto secondUser = UserDto.builder()
+                .username("John")
+                .id(0L)
+                .build();
+        UserDto firstUser = UserDto.builder()
+                .username("Lopux")
+                .id(1L)
+                .build();
+
+        List<UserDto> participants = Arrays.asList(secondUser, firstUser);
+        when(eventParticipationService.getParticipants(eventId)).thenReturn(participants);
+        List<UserDto> result = eventParticipationController.getParticipants(eventId);
+
+        assertNotNull(result);
+        assertEquals(2, result.size());
+        assertEquals("John", result.get(0).getUsername());
+        assertEquals("Lopux", result.get(1).getUsername());
+
+        verify(eventParticipationService, times(1)).getParticipants(eventId);
+    }
+
+    @Test
+    void getParticipants_InvalidId() {
+        long invalidEventId = -1L;
+
+        DataValidationException exception = assertThrows(DataValidationException.class, () -> {
+            eventParticipationController.getParticipants(invalidEventId);
+        });
+
+        assertEquals("Id is less than zero", exception.getMessage());
+        verify(eventParticipationService, never()).getParticipants(anyLong());
+    }
+
+    @Test
+    void getParticipantsCount() throws Exception {
+        long eventId = 1L;
+        int expectedCount = 5;
+        when(eventParticipationService.getParticipantsCount(eventId)).thenReturn(expectedCount);
+
+        mockMvc.perform(get("/events/{eventId}/count_participants", eventId))
+                .andExpect(status().isOk())
+                .andExpect(content().string(String.valueOf(expectedCount)));
+
+        verify(eventParticipationService, times(1)).getParticipantsCount(eventId);
+    }
+
+    @Test
+    void getParticipantsCount_InvalidId() {
+        long eventId = -1L;
+
+        DataValidationException exception = assertThrows(DataValidationException.class, () -> {
+            eventParticipationController.getParticipantsCount(eventId);
+        });
+
+        assertEquals("Id is less than zero", exception.getMessage());
+        verify(eventParticipationService, never()).getParticipantsCount(anyLong());
+
     }
 }
