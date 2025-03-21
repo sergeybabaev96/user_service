f2d4dd5b39ded9608909afa64986fbdfe154ff86
pstratonni <79229338+pstratonni@users.noreply.github.com>
2025-03-21 22:01:32 +0100

    Corrected

--- src/main/java/school/faang/user_service/utils/ValidationUtils.java
+++ src/main/java/school/faang/user_service/utils/ValidationUtils.java
@@ -1,12 +1,31 @@
 package school.faang.user_service.utils;
 
+import school.faang.user_service.dto.event.EventDto;
 import school.faang.user_service.dto.skill.SkillDto;
 import school.faang.user_service.exception.DataValidationException;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class ValidationUtils {
     public static void validateSkill(SkillDto skill) {
         if (skill == null || skill.title().isBlank()) {
-            throw new DataValidationException("Title doesn't allow be empty");}
+            throw new DataValidationException("Title doesn't allow be empty");
+        }
+    }
 
+    public static void validateEvent(EventDto event) {
+        if (event == null || event.getTitle().isBlank() || !Objects.nonNull(event.getStartDate())
+                || event.getStartDate().isBefore(LocalDateTime.now())
+                || event.getOwnerId() == null) {
+            throw new DataValidationException("Event not confirmed");
+        }
+    }
+
+    public static void validateEventId(Long id) {
+        if (id == null || id <= 0) {
+            throw new DataValidationException("Event id not valid");
+        }
     }
 }
+
