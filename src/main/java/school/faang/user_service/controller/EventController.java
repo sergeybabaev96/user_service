cf9cb7a80cb3ba80e6cc461a5a4ddc40397ec82b
pstratonni <79229338+pstratonni@users.noreply.github.com>
2025-03-21 22:01:32 +0100

    Corrected

--- src/main/java/school/faang/user_service/controller/EventController.java
+++ src/main/java/school/faang/user_service/controller/EventController.java
@@ -4,12 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import school.faang.user_service.dto.event.EventDto;
 import school.faang.user_service.dto.event.EventFilterDto;
-import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.service.event.EventService;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Objects;
+
+import static school.faang.user_service.utils.ValidationUtils.validateEvent;
+import static school.faang.user_service.utils.ValidationUtils.validateEventId;
 
 @Controller
 @RequiredArgsConstructor
@@ -44,18 +44,5 @@ public class EventController {
         return eventService.getParticipatedEvents(userId);
     }
 
-    private void validateEvent(EventDto event) {
-        if (event == null || event.getTitle().isBlank() || !Objects.nonNull(event.getStartDate())
-                || event.getStartDate().isBefore(LocalDateTime.now())
-                || event.getOwnerId() == null ) {
-            throw new DataValidationException("Event not confirmed");
-        }
-    }
-
-    private void validateEventId(Long id) {
-        if(id == null || id <= 0) {
-            throw new DataValidationException("Event id not valid");
-        }
-    }
 
 }
--- src/main/java/school/faang/user_service/dto/event/EventDto.java
+++ src/main/java/school/faang/user_service/dto/event/EventDto.java
@@ -1,23 +1,24 @@
 package school.faang.user_service.dto.event;
 
-import lombok.Data;
+
 import school.faang.user_service.entity.event.EventStatus;
 import school.faang.user_service.entity.event.EventType;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Data
-public class EventDto {
-    private Long id;
-    private String title;
-    private LocalDateTime startDate;
-    private LocalDateTime endDate;
-    private Long ownerId;
-    private String description;
-    private List<Long> relatedSkills;
-    private String location;
-    private int maxAttendees;
-    private EventType eventType;
-    private EventStatus eventStatus;
+
+public record EventDto(
+        Long id,
+        String title,
+        LocalDateTime startDate,
+        LocalDateTime endDate,
+        Long ownerId,
+        String description,
+        List<Long> relatedSkills,
+        String location,
+        int maxAttendees,
+        EventType eventType,
+        EventStatus eventStatus
+) {
 }
