b9379afa5d2e67d2123efd1404720a14a4efeaad
saroyangor <dr.saroyan98@gmail.com>
2025-03-21 18:22:49 +0300

    BJS2-65172: Deactivate User

--- src/main/java/school/faang/user_service/controller/UserController.java
+++ src/main/java/school/faang/user_service/controller/UserController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +13,10 @@ import school.faang.user_service.service.UserService;
 
 import java.util.List;
 
-@RestController
 @RequiredArgsConstructor
+@RestController
 @RequestMapping("/users")
 public class UserController {
-
     private final UserService userService;
 
     @GetMapping("/{userId}")
@@ -27,5 +27,15 @@ public class UserController {
     @PostMapping
     List<UserDto> getUsersByIds(@RequestBody List<Long> ids) {
         return userService.getUsersByIds(ids);
+    }
+
+    @PutMapping("/deactivate/{id}")
+    public UserDto deactivateUser(@PathVariable Long id) {
+        return userService.deactivateUser(id);
+    }
+
+    @PutMapping("/activate/{id}")
+    public UserDto activateUser(@PathVariable Long id) {
+        return userService.activateUser(id);
     }
 }
