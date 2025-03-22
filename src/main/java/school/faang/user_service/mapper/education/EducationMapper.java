432a1fabb5c9a3801d08374d4b700750cc52ea7d
Alexti25 <alxej.kazakov@gmail.com>
2025-03-22 13:08:42 +0300

    добавил unmappedTargetPolicy = ReportingPolicy.IGNORE в маппер

--- src/main/java/school/faang/user_service/mapper/education/EducationMapper.java
+++ src/main/java/school/faang/user_service/mapper/education/EducationMapper.java
@@ -1,10 +1,11 @@
 package school.faang.user_service.mapper.education;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 import school.faang.user_service.dto.education.EducationDto;
 import school.faang.user_service.entity.Education;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface EducationMapper {
 
     Education toEducation(EducationDto educationDto);
