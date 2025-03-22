67bb833f6bbbc07a61dcdf041e6435cb241d42b1
saroyangor <dr.saroyan98@gmail.com>
2025-03-23 02:53:42 +0300

    fix comments, delete other repositories from UserService, add all logic in corresponding services

--- src/main/java/school/faang/user_service/service/UserService.java
+++ src/main/java/school/faang/user_service/service/UserService.java
@@ -3,23 +3,32 @@ package school.faang.user_service.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import school.faang.user_service.dto.UserDto;
+import school.faang.user_service.dto.event.EventDTO;
+import school.faang.user_service.dto.goal.GoalDto;
 import school.faang.user_service.entity.User;
 import school.faang.user_service.mapper.UserMapper;
+import school.faang.user_service.mapper.goal.GoalMapper;
 import school.faang.user_service.repository.UserRepository;
-import school.faang.user_service.repository.event.EventRepository;
-import school.faang.user_service.repository.goal.GoalRepository;
+import school.faang.user_service.service.event.EventService;
+import school.faang.user_service.service.goal.GoalService;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    private final UserRepository userRepository;
-    private final GoalRepository goalRepository;
-    private final EventRepository eventRepository;
-    private final MentorshipService mentorshipService;
     private final UserMapper userMapper;
+    private final GoalMapper goalMapper;
+    private final GoalService goalService;
+    private final EventService eventService;
+    private final MentorshipService mentorshipService;
+    private final UserRepository userRepository;
+
+    private static final int MONTHS = 3;
 
     public User getUserById(long id) {
         return userRepository.findById(id).orElseThrow(()
@@ -39,7 +48,7 @@ public class UserService {
                 () -> new IllegalArgumentException("User with id " + id + " not found")
         );
 
-        if (user.getUpdatedAt().isAfter(LocalDateTime.now().minusMonths(3))) {
+        if (user.getUpdatedAt().isAfter(LocalDateTime.now().minusMonths(MONTHS))) {
             user.setActive(true);
         }
 
@@ -51,33 +60,69 @@ public class UserService {
                 () -> new IllegalArgumentException("User with id " + id + " not found")
         );
 
-        deleteGoalsAndEvents(user);
+        stopUserGoals(id);
+        stopUserEvents(id);
 
         user.setActive(false);
 
-        user.getMentees().forEach(mentee -> mentorshipService.deleteMentorship(user.getId(), mentee.getId()));
+        deleteMentorship(id);
 
         return userMapper.toDto(userRepository.save(user));
     }
 
-    private void deleteGoalsAndEvents(User user) {
-        user.getGoals().forEach(goal -> {
-            user.getGoals().remove(goal);
+    private void stopUserGoals(Long userId) {
+        List<Long> userGoalsForDeleting = new ArrayList<>();
+        List<Long> userGoalsForUpdating = new ArrayList<>();
 
-            goal.getUsers().remove(user);
-            goalRepository.save(goal);
-            if (goal.getUsers().isEmpty()) {
-                goalRepository.deleteById(goal.getId());
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
+
+        List<GoalDto> allGoals = user.getGoals().stream().map(goalMapper::goalToGoalDto).toList();
+
+        for (GoalDto goal : allGoals) {
+            if (shouldGoalBeDeleted(goal, userId)) {
+                userGoalsForDeleting.add(goal.id());
+            } else {
+                userGoalsForUpdating.add(goal.id());
             }
+        }
 
-            userRepository.save(user);
-        });
+        goalService.deleteAllByIds(userGoalsForDeleting);
+        goalService.removeUserFromGoals(userGoalsForUpdating, userId);
+    }
 
-        user.getOwnedEvents().forEach(event -> {
-            user.getOwnedEvents().remove(event);
-            userRepository.save(user);
+    private void stopUserEvents(Long userId) {
+        List<Long> userEventsForDeleting = new ArrayList<>();
+        List<Long> userEventsForUpdating = new ArrayList<>();
 
-            eventRepository.deleteById(event.getId());
-        });
+        List<EventDTO> allEvents = eventService.getParticipatedEvents(userId);
+
+        for (EventDTO event : allEvents) {
+            if (shouldEventBeDeleted(event, userId)) {
+                userEventsForDeleting.add(event.getId());
+            } else {
+                userEventsForUpdating.add(event.getId());
+            }
+        }
+
+        eventService.deleteAllByIds(userEventsForDeleting);
+        eventService.removeUserFromEvents(userEventsForUpdating, userId);
+    }
+
+    private void deleteMentorship(Long userId) {
+        mentorshipService.deleteMentorship(userId);
+    }
+
+    private boolean shouldGoalBeDeleted(GoalDto goal, Long userId) {
+        List<Long> userIds = goal.userIds();
+        return userIds.size() == 1 && Objects.equals(userIds.get(0), userId);
+    }
+
+    private boolean shouldEventBeDeleted(EventDTO event, Long userId) {
+        if (!Objects.equals(event.getOwnerId(), userId)) {
+            return false;
+        }
+
+        List<Long> userIds = event.getAttendeesIds();
+        return userIds.size() == 1 && Objects.equals(userIds.get(0), userId);
     }
 }
