6d213453fbb5aa48ad86f2143152c9835db48cf5
Kadiros9927 <65703156+Kadiros9927@users.noreply.github.com>
2025-03-23 11:14:13 +0300

    Поправил предыдущий конфликт в UserMapper.java

--- src/main/java/school/faang/user_service/mapper/UserMapper.java
+++ src/main/java/school/faang/user_service/mapper/UserMapper.java
@@ -4,10 +4,11 @@ import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 import school.faang.user_service.dto.user.UserDto;
 import school.faang.user_service.entity.User;
+import school.faang.user_service.dto.FollowerResponseDto;
 
 @Mapper(componentModel = "spring", unmappedSourcePolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
-
+    FollowerResponseDto userToUserDto(User user);
     User toEntity(UserDto userDto);
     UserDto toDto(User entity);
 }
