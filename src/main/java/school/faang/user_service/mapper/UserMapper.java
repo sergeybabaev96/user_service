052fd9b3db0da0a1782f4f23232cd43d9178c8e2
Natalya Vasileva <boryantheone228@yandex.ru>
2025-03-24 07:51:15 +0300

    BJS2-66892 Хранение запросов и операций

--- src/main/java/school/faang/user_service/mapper/UserMapper.java
+++ src/main/java/school/faang/user_service/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package school.faang.user_service.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import school.faang.user_service.dto.user.UserReadDto;
 import school.faang.user_service.entity.User;
@@ -10,5 +11,6 @@ public interface UserMapper {
 
     User toEntity(UserReadDto dto);
 
+    @Mapping(source = "contactPreference.preference", target = "preference")
     UserReadDto toDto(User entity);
 }
