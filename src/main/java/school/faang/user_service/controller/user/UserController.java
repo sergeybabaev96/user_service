4d5715ae446eac293ce837173458fe4b0303b997
Marsel <mars1212@inbox.ru>
2025-03-24 22:24:02 +0300

    BJS2-67829 добавил метод для работы  с юзером из других сервисов

---
+++ src/main/java/school/faang/user_service/controller/user/UserController.java
@@ -0,0 +1,22 @@
+package school.faang.user_service.controller.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import school.faang.user_service.dto.UserViewDto;
+import school.faang.user_service.service.UserService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
+
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/{userId}")
+    public UserViewDto getUser(@PathVariable long userId) {
+        return userService.getUserForOtherService(userId);
+    }
+}
