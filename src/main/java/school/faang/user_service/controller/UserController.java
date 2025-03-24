e8c47a9e0142bbd58b1f27d9bfa1bee26a39b902
pstratonni <79229338+pstratonni@users.noreply.github.com>
2025-03-25 00:12:40 +0100

    Task BJS2-66082 Like system. Related branch in post_service

---
+++ src/main/java/school/faang/user_service/controller/UserController.java
@@ -0,0 +1,25 @@
+package school.faang.user_service.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import school.faang.user_service.dto.UserDto;
+import school.faang.user_service.service.UserService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class UserController {
+
+    private final UserService userService;
+
+
+
+    @GetMapping("/user/{id}")
+    public UserDto findUserAndReturnDto(@PathVariable Long id) {
+
+        return userService.findUserAndReturnDto(id);
+    }
+}
--- src/main/java/school/faang/user_service/service/UserService.java
+++ src/main/java/school/faang/user_service/service/UserService.java
@@ -4,8 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataRetrievalFailureException;
 import org.springframework.stereotype.Service;
+import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.entity.User;
 import school.faang.user_service.exception.DataValidationException;
+import school.faang.user_service.mapper.UserMapper;
 import school.faang.user_service.repository.UserRepository;
 
 @Slf4j
@@ -13,6 +15,7 @@ import school.faang.user_service.repository.UserRepository;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     public boolean doesUserExist(long userId) {
         return userRepository.existsById(userId);
@@ -38,5 +41,10 @@ public class UserService {
 
     public boolean existsById(long userId) {
         return userRepository.existsById(userId);
+    }
+
+    public UserDto findUserAndReturnDto(Long userId){
+        User user = getUserById(userId);
+        return userMapper.toDto(user);
     }
 }
