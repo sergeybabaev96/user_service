2ed1b2ef717f8b3380f038aa03ea6e02644541f2
User Rody4457 <rodyon0212@gmail.com>
2025-03-25 10:20:33 +0400

    внес исправления

--- src/main/java/school/faang/user_service/exception/data_validation_exception/DataValidationException.java
+++ src/main/java/school/faang/user_service/exception/data_validation_exception/DataValidationException.java
@@ -6,7 +6,7 @@ public class DataValidationException extends RuntimeException {
         super(message);
     }
 
-    public static final String TIME_EXCEPTION = "time validation failed";
+    public static final String TIME_EXCEPTION = "time validation is failed";
     public static final String USER_DOESNT_EXIST = "user does not exist";
     public static final String WORK_SCHEDULE_DOESNT_EXIST = "work schedule does not exist";
     public static final String USER_DOESNT_HAVE_ACCESS = "you does not have access";
