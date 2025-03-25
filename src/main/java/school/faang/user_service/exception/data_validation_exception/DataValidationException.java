ced150bc42aa737c6a33238bc17321012090186d
User Rody4457 <rodyon0212@gmail.com>
2025-03-25 10:19:49 +0400

    удалено лишнее

--- src/main/java/school/faang/user_service/exception/data_validation_exception/DataValidationException.java
+++ src/main/java/school/faang/user_service/exception/data_validation_exception/DataValidationException.java
@@ -2,12 +2,13 @@ package school.faang.user_service.exception.data_validation_exception;
 
 public class DataValidationException extends RuntimeException {
 
+    public DataValidationException(String message) {
+        super(message);
+    }
+
     public static final String TIME_EXCEPTION = "time validation failed";
     public static final String USER_DOESNT_EXIST = "user does not exist";
     public static final String WORK_SCHEDULE_DOESNT_EXIST = "work schedule does not exist";
     public static final String USER_DOESNT_HAVE_ACCESS = "you does not have access";
 
-    public DataValidationException(String message) {
-        super(message);
-    }
 }
