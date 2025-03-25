bc4e529726e02c310fd9f9480b2026ed050270e8
s3nkyh <sherlok.pupkin@yandex.ru>
2025-03-25 22:47:17 +0300

    Покрыть код задачи "Написание рекомендации" unit-тестами (#2819)
    * Покрыл код задачи "Написание рекомендации" unit-тестами,
    также немного поменял логику в RecommendationService
    
    * Исправил аннотации в RecommendationController,
    Добавил аннотации в дто,
    Вынес повторяющийся код в @BeforeEach
    
    * добавил @RequestMapping("/recommendation")
    
    ---------
    
    Co-authored-by: Yaroslav Olkin <shrelok.pupkin@yandex.ru>

--- src/main/java/school/faang/user_service/controller/RecommendationController.java
+++ src/main/java/school/faang/user_service/controller/RecommendationController.java
@@ -2,8 +2,14 @@ package school.faang.user_service.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.recommendation.RecommendationDto;
 import school.faang.user_service.service.RecommendationService;
 
@@ -11,36 +17,42 @@ import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
-@Controller
+@RestController
+@RequestMapping("/recommendation")
 public class RecommendationController {
     private final RecommendationService recommendationService;
 
-    public RecommendationDto giveRecommendation(RecommendationDto recommendationDto) {
+    @PostMapping()
+    public RecommendationDto giveRecommendation(@RequestBody RecommendationDto recommendationDto) {
         log.info("The process of transmitting the recommendation to the user begins");
         RecommendationDto returnedRecommendation = recommendationService.create(recommendationDto);
         log.info("The recommendation has been passed");
         return returnedRecommendation;
     }
 
-    public RecommendationDto updateRecommendation(RecommendationDto recommendationDto) {
+    @PutMapping()
+    public RecommendationDto updateRecommendation(@RequestBody RecommendationDto recommendationDto) {
         log.info("Starting update of recommendation");
         RecommendationDto updatedRecommendationDto = recommendationService.update(recommendationDto);
         log.info("The update is finished");
         return updatedRecommendationDto;
     }
 
-    public void deleteRecommendation(Long id) {
+    @DeleteMapping("/{id}")
+    public void deleteRecommendation(@PathVariable Long id) {
         log.info("Starting to delete the recommendation");
         recommendationService.delete(id);
         log.info("The recommendation has been removed");
     }
 
-    public List<RecommendationDto> getAllGivenRecommendation(Long authorId) {
+    @GetMapping("/{authorId}")
+    public List<RecommendationDto> getAllGivenRecommendation(@PathVariable Long authorId) {
         outputOfRecommendations(recommendationService.getAllGivenRecommendation(authorId));
         return recommendationService.getAllGivenRecommendation(authorId);
     }
 
-    public List<RecommendationDto> getAllUserRecommendations(Long receiverId) {
+    @GetMapping("/{receiverId}")
+    public List<RecommendationDto> getAllUserRecommendations(@PathVariable Long receiverId) {
         outputOfRecommendations(recommendationService.getAllUserRecommendations(receiverId));
         return recommendationService.getAllGivenRecommendation(receiverId);
     }
--- src/main/java/school/faang/user_service/dto/recommendation/SkillOfferDto.java
+++ src/main/java/school/faang/user_service/dto/recommendation/SkillOfferDto.java
@@ -1,8 +1,15 @@
 package school.faang.user_service.dto.recommendation;
 
-import lombok.Data;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SkillOfferDto {
     private Long id;
     private Long skillId;
