fd10964a07a84b1c3ea20c061931fa043723548a
pstratonni <79229338+pstratonni@users.noreply.github.com>
2025-03-30 21:12:49 +0200

    corrected ErrorResponse

--- src/main/java/school/faang/user_service/exception/ErrorResponse.java
+++ src/main/java/school/faang/user_service/exception/ErrorResponse.java
@@ -2,19 +2,19 @@ package school.faang.user_service.exception;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
+import org.joda.time.LocalDateTime;
 
-import java.util.Date;
 
 @Getter
 public class ErrorResponse {
     private final String message;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "UTC")
-    private Date timestamp;
+    private LocalDateTime timestamp;
 
     public ErrorResponse(String message) {
         this.message = message;
-        this.timestamp = new Date();
+        this.timestamp = LocalDateTime.now();
     }
 
 }
