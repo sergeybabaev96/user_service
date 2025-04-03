ec0297a1b80ea3a5bf102174377bbda333edee4c
sunmajor <makar.a.v100@gmail.com>
2025-04-03 21:53:24 +0300

    Образование. После исправлений

--- src/main/java/school/faang/user_service/entity/Education.java
+++ src/main/java/school/faang/user_service/entity/Education.java
@@ -10,16 +10,19 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import school.faang.user_service.dto.EducationDto;
 
+@Data
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "education")
-public class Education {
+public class Education extends EducationDto {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
