88037bfb228e049a027198e8052c09d4dca41d6a
pstratonni <79229338+pstratonni@users.noreply.github.com>
2025-04-06 17:17:51 +0200

    commit

--- src/main/java/school/faang/user_service/UserServiceApplication.java
+++ src/main/java/school/faang/user_service/UserServiceApplication.java
@@ -5,13 +5,20 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients("school.faang.user_service.client")
 @EnableScheduling
+@EnableAsync
+@ConfigurationPropertiesScan
 public class UserServiceApplication {
 
     public static void main(String[] args) {
