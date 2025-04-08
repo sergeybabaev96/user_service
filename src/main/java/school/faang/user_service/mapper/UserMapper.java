534f495c4cd298247b646cd220879a4359b2e354
IgnatovEI <ignatovei96@mail.ru>
2025-04-08 21:19:06 +0300

    fix conflicts

--- src/main/java/school/faang/user_service/mapper/UserMapper.java
+++ src/main/java/school/faang/user_service/mapper/UserMapper.java
@@ -1,14 +1,21 @@
 package school.faang.user_service.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import school.faang.user_service.dto.UserDto;
+import school.faang.user_service.dto.FollowerResponseDto;
 import school.faang.user_service.entity.User;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+
+    @Mapping(source = "username", target = "userName")
     UserDto toDto(User user);
 
+    FollowerResponseDto userToUserDto(User user);
+
     List<UserDto> toDtoList(List<User> users);
+
 }
