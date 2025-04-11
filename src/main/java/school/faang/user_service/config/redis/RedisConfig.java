a123f6b8cf565fdf907a53b3c6fe045b5df73e11
Evgeny Morzeev <morzeevevg@gmail.com>
2025-04-12 01:31:02 +0300

    feature/BJS2-65647 redis config for subscription

--- src/main/java/school/faang/user_service/config/redis/RedisConfig.java
+++ src/main/java/school/faang/user_service/config/redis/RedisConfig.java
@@ -1,8 +1,6 @@
 package school.faang.user_service.config.redis;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
