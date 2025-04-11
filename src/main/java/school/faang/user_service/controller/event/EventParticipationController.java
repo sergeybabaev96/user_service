a212ae3e5bf05baec0c777266947607783eff860
Ilhan Mustafa <ilhnmst@gmail.com>
2025-04-11 16:52:59 +0300

    Refactor

--- src/main/java/school/faang/user_service/controller/event/EventParticipationController.java
+++ src/main/java/school/faang/user_service/controller/event/EventParticipationController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +21,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/events")
-//@Tag(name = "Event Participation", description = "API for managing event participations")
+@Tag(name = "Event Participation", description = "API for managing event participations")
 public class EventParticipationController {
     private final EventParticipationService eventParticipationService;
 
