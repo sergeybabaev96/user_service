4e10f71f65d8b33f60e6d62a47e406635ece7ab0
Yaroslav Olkin <shrelok.pupkin@yandex.ru>
2025-04-13 11:50:05 +0300

    Добавил в UserDto PreferredContact

--- src/main/java/school/faang/user_service/dto/UserDto.java
+++ src/main/java/school/faang/user_service/dto/UserDto.java
@@ -1,8 +1,11 @@
 package school.faang.user_service.dto;
 
+import school.faang.user_service.entity.contact.PreferredContact;
+
 public record UserDto(
         Long id,
         String username,
-        String email
+        String email,
+        PreferredContact preference
 ) {
 }
