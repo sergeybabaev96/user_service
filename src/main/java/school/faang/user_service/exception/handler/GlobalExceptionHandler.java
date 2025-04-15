901aebebf9da784a719bc8214df1ff661a5ff6d6
SergeyErmoshenko <ermoshenko.work@gmail.com>
2025-04-15 14:06:42 +0300

    хэндлер

--- src/main/java/school/faang/user_service/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/school/faang/user_service/exception/handler/GlobalExceptionHandler.java
@@ -5,36 +5,78 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import school.faang.user_service.exception.PaymentFailedException;
+import school.faang.user_service.exception.PremiumAlreadyPurchasedException;
+import school.faang.user_service.exception.PremiumNotActiveException;
 import school.faang.user_service.exception.PremiumPaymentReplyNotReceivedException;
 import school.faang.user_service.exception.PremiumPriceReplyNotReceivedException;
 
 import static school.faang.user_service.messages.ErrorMessages.NO_PREMIUM_PAYMENT_RESPONSE_RECEIVED_FROM_PAYMENT_SERVICE;
 import static school.faang.user_service.messages.ErrorMessages.NO_PREMIUM_PRICE_RESPONSE_RECEIVED_FROM_PAYMENT_SERVICE;
+import static school.faang.user_service.messages.ErrorMessages.THE_PAYMENT_ATTEMPT_WAS_UNSUCCESSFUL;
+import static school.faang.user_service.messages.ErrorMessages.THE_USER_DOES_NOT_HAVE_AN_ACTIVE_PREMIUM_SUBSCRIPTION;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(PremiumPriceReplyNotReceivedException.class)
-    public ResponseEntity<ErrorResponse> handlePremiumReplyNotReceivedException(PremiumPriceReplyNotReceivedException exception) {
-        String message = NO_PREMIUM_PRICE_RESPONSE_RECEIVED_FROM_PAYMENT_SERVICE;
-        ErrorResponse errorResponse = ErrorResponse.builder(exception,
-                        HttpStatus.GATEWAY_TIMEOUT, exception.getMessage())
+    public ResponseEntity<ErrorResponse> handlePremiumReplyNotReceivedException(
+            PremiumPriceReplyNotReceivedException exception) {
+
+        ErrorResponse errorResponse = ErrorResponse.builder(exception, HttpStatus.GATEWAY_TIMEOUT,
+                        exception.getMessage())
                 .title("Something went wrong with payment service.")
-                .detail(message)
+                .detail(NO_PREMIUM_PRICE_RESPONSE_RECEIVED_FROM_PAYMENT_SERVICE)
                 .property("service", "payment")
                 .build();
         return new ResponseEntity<>(errorResponse, HttpStatus.GATEWAY_TIMEOUT);
     }
 
     @ExceptionHandler(PremiumPaymentReplyNotReceivedException.class)
-    public ResponseEntity<ErrorResponse> handlePremiumPaymentReplyNotReceivedException(PremiumPaymentReplyNotReceivedException exception) {
-        String message = NO_PREMIUM_PAYMENT_RESPONSE_RECEIVED_FROM_PAYMENT_SERVICE;
-        ErrorResponse errorResponse = ErrorResponse.builder(exception,
-                        HttpStatus.GATEWAY_TIMEOUT, exception.getMessage())
+    public ResponseEntity<ErrorResponse> handlePremiumPaymentReplyNotReceivedException(
+            PremiumPaymentReplyNotReceivedException exception) {
+
+        ErrorResponse errorResponse = ErrorResponse.builder(exception, HttpStatus.GATEWAY_TIMEOUT, exception.getMessage())
                 .title("Something went wrong with payment service.")
-                .detail(message)
+                .detail(NO_PREMIUM_PAYMENT_RESPONSE_RECEIVED_FROM_PAYMENT_SERVICE)
                 .property("service", "payment")
                 .build();
         return new ResponseEntity<>(errorResponse, HttpStatus.GATEWAY_TIMEOUT);
+    }
+
+    @ExceptionHandler(PremiumAlreadyPurchasedException.class)
+    public ResponseEntity<ErrorResponse> handlePremiumAlreadyPurchasedException(
+            PremiumAlreadyPurchasedException exception) {
+
+        ErrorResponse errorResponse = ErrorResponse.builder(exception, HttpStatus.BAD_REQUEST, exception.getMessage())
+                .title("Premium already purchased.")
+                .detail("The user has already purchased the premium plan.")
+                .property("service", "premium")
+                .build();
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(PremiumNotActiveException.class)
+    public ResponseEntity<ErrorResponse> handlePremiumNotActiveException(
+            PremiumNotActiveException exception) {
+
+        ErrorResponse errorResponse = ErrorResponse.builder(exception, HttpStatus.BAD_REQUEST, exception.getMessage())
+                .title("Premium already purchased.")
+                .detail(THE_USER_DOES_NOT_HAVE_AN_ACTIVE_PREMIUM_SUBSCRIPTION)
+                .property("service", "premium")
+                .build();
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(PaymentFailedException.class)
+    public ResponseEntity<ErrorResponse> handlePaymentFailedException(
+            PaymentFailedException exception) {
+
+        ErrorResponse errorResponse = ErrorResponse.builder(exception, HttpStatus.BAD_REQUEST, exception.getMessage())
+                .title("Payment failed.")
+                .detail(THE_PAYMENT_ATTEMPT_WAS_UNSUCCESSFUL)
+                .property("service", "payment")
+                .build();
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 }
