56ad08c917fe3954e427a482460d62ff5c804f88
Ilyas Azhdar <azhdar902@gmail.com>
2025-04-25 18:23:23 +0500

    added CommentBanListener

--- src/main/java/school/faang/user_service/exception/ConvertingDataException.java
+++ src/main/java/school/faang/user_service/exception/ConvertingDataException.java
@@ -5,4 +5,8 @@ public class ConvertingDataException extends RuntimeException {
     public ConvertingDataException(String message, Object... args) {
         super(String.format(message, args));
     }
+
+    public ConvertingDataException(String message, Throwable cause, Object... args) {
+        super(String.format(message, args), cause);
+    }
 }
