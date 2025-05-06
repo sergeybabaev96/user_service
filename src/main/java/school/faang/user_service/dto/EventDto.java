fbf31d22a21871a80742949c9e3f42c857cb041f
Raidanmark <Onidanil@Gmail.com>
2025-05-06 13:54:42 +0200

    EventDto created

---
+++ src/main/java/school/faang/user_service/dto/EventDto.java
@@ -0,0 +1,20 @@
+package school.faang.user_service.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class EventDto {
+    private long id;
+    private String title;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private Long ownerId;
+    private String description;
+    private List<Long> relatedSkills;
+    private String location;
+    private int maxAttendees;
+    private String eventType;
+}
