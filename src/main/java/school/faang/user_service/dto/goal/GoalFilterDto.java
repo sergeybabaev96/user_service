8021c99936201ba1e5b79816da4e1e823a07b9d6
Aleksandr <alexandr.sidorkin@gmail.com>
2025-05-06 19:49:04 +0200

    Task 77372 Постановка целей Builder Fix

--- src/main/java/school/faang/user_service/dto/goal/GoalFilterDto.java
+++ src/main/java/school/faang/user_service/dto/goal/GoalFilterDto.java
@@ -1,6 +1,8 @@
 package school.faang.user_service.dto.goal;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 import school.faang.user_service.entity.Skill;
 import school.faang.user_service.entity.goal.Goal;
 import school.faang.user_service.entity.goal.GoalStatus;
@@ -8,10 +10,9 @@ import school.faang.user_service.entity.goal.GoalStatus;
 import java.util.List;
 
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class GoalFilterDto {
-
-    public GoalFilterDto() {
-    }
 
     private String description;
     private String title;
