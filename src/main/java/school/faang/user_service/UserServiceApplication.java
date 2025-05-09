c402bcf8a99990577908bf9e17e4149bff0292c2
Stanislaff Fedyanin <re1971reg@gmail.com>
2025-05-09 21:47:47 +0300

    create request Recommendation. first task. without tests.

--- src/main/java/school/faang/user_service/UserServiceApplication.java
+++ src/main/java/school/faang/user_service/UserServiceApplication.java
@@ -3,13 +3,11 @@ package school.faang.user_service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
-@Slf4j
 @SpringBootApplication
 @EnableFeignClients("school.faang.user_service.client")
 public class UserServiceApplication {
