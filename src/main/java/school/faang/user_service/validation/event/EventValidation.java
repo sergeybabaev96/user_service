71dfac650d4adc5d0fdf3572939258d0fd479b90
Gafurova Faribo <faribo.gafur@gmail.com>
2025-05-10 16:56:35 +0300

    вынесены: * документация из API * валидация в отдельный класс

---
+++ src/main/java/school/faang/user_service/validation/event/EventValidation.java
@@ -0,0 +1,25 @@
+package school.faang.user_service.validation.event;
+
+import org.springframework.stereotype.Component;
+import school.faang.user_service.exception.EventCreationNotAllowedException;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Component
+public class EventValidation {
+    public void validateUserHasAllEventSkills(List<Long> eventSkillsIds, Set<Long> ownersSkillsIds) {
+        if (eventSkillsIds != null && !eventSkillsIds.isEmpty()) {
+            Set<Long> requiredSkillsIds = new HashSet<>(eventSkillsIds);
+            requiredSkillsIds.removeAll(ownersSkillsIds);
+
+            if (!requiredSkillsIds.isEmpty()) {
+                throw new EventCreationNotAllowedException(
+                        String.format("Недостаточно навыков для создания данного мероприятия. Отсутствуют навыки: %s",
+                                requiredSkillsIds)
+                );
+            }
+        }
+    }
+}
