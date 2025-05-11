84c31548e079d3faa5b5d6cf4fbbed04cb264c8d
AndreyK <you@example.com>
2025-05-11 23:10:37 +0300

    BJS2-77778: реализовано согласно заданию Участвовать в событии.

---
+++ src/main/java/school/faang/user_service/service/event/EventParticipationService.java
@@ -0,0 +1,45 @@
+package school.faang.user_service.service.event;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import school.faang.user_service.entity.User;
+import school.faang.user_service.exception.DataValidationException;
+import school.faang.user_service.repository.event.EventParticipationRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EventParticipationService {
+    private final EventParticipationRepository eventParticipationRepository;
+
+    public void registerParticipant(long eventId, long userId) {
+        if (isParticipantRegistered(eventId, userId)) {
+            throw new DataValidationException("User %d already registered for event %d".formatted(userId, eventId));
+        } else {
+            eventParticipationRepository.register(eventId, userId);
+        }
+    }
+
+    public void unregisterParticipant(long eventId, long userId) {
+        if (isParticipantRegistered(eventId, userId)) {
+            eventParticipationRepository.unregister(eventId, userId);
+        } else {
+            throw new DataValidationException("User %d not registered for event %d".formatted(userId, eventId));
+        }
+    }
+
+    public List<User> getParticipant(long eventId) {
+        return eventParticipationRepository.findAllParticipantsByEventId(eventId);
+    }
+
+    public long getParticipantsCount(long eventId) {
+        return eventParticipationRepository.countParticipants(eventId);
+    }
+
+    private boolean isParticipantRegistered(long eventId, long userId) {
+        return eventParticipationRepository.findAllParticipantsByEventId(eventId)
+                .stream()
+                .anyMatch(user -> user.getId() == userId);
+    }
+}
