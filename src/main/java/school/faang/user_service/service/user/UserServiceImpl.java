235231faf05f5acc265d8a82dbd284c360393571
Aleksandr <alexandr.sidorkin@gmail.com>
2025-05-12 13:51:07 +0200

    Task 77372 рефакторинг: Введён бин маппер-контекста чтобы уйти от проброса в маппер сервисов
    Переименованы методы маппера
    
    Goal entity убран из контроллера
    
    Константа на максимальное количество активных целей вынесена в application.yaml
    
    Перенос статических методов Goal.isActive в утил-класс

--- src/main/java/school/faang/user_service/service/user/UserServiceImpl.java
+++ src/main/java/school/faang/user_service/service/user/UserServiceImpl.java
@@ -2,7 +2,6 @@ package school.faang.user_service.service.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import school.faang.user_service.UserServiceApplication;
 import school.faang.user_service.entity.User;
 import school.faang.user_service.repository.UserRepository;
 import school.faang.user_service.service.UserService;
